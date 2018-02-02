92c37138b968cf974294500ab3672ba275ee5c0a
guohouxiao <122492508@qq.com>
2018-02-02 11:34:10 +0800

    first commit

---
+++ PAT (Advanced Level) Practise/1001. A+B Format (20).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(a + b < 0)
+        cout << "-";
+    string ret = to_string(abs(a + b));
+    while(ret.size() > 0) {
+        int pos = (ret.size() % 3 == 0) ? 3 : ret.size() % 3;
+        cout << ret.substr(0, pos);
+        if(ret.size() > 3)
+            cout << ",";
+        ret = ret.substr(pos);
+    }
+    return 0;
+}
