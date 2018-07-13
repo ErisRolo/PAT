9409d18b2cb65818d31281bbc4dc19fd63f97103
guohouxiao <122492508@qq.com>
2018-07-13 22:53:13 +0800

    update 1001-advanced

--- PAT (Advanced Level) Practise/1001. A+B Format (20).cpp
+++ PAT (Advanced Level) Practise/1001. A+B Format (20).cpp
@@ -1,18 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <cstdio>
+
+int num[10];
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if(a + b < 0)
-        cout << "-";
-    string ret = to_string(abs(a + b));
-    while(ret.size() > 0) {
-        int pos = (ret.size() % 3 == 0) ? 3 : ret.size() % 3;
-        cout << ret.substr(0, pos);
-        if(ret.size() > 3)
-            cout << ",";
-        ret = ret.substr(pos);
+    int a, b, sum;
+    scanf("%d%d", &a, &b);
+    sum = a + b; //将a+b赋值给sum
+    if(sum < 0) {
+        //sum为负数时，输出负号并取sum的相反数
+        printf("-");
+        sum = -sum;
+    }
+    int len = 0; //len存放sum的长度
+    if(sum == 0)
+        num[len++] = 0; //sum为0时特殊处理
+    //将sum存入数组num[]中，其中sum的低位存放到num[]的低位
+    while(sum) {
+        //将sum的末位sum%10存放到num[len]，然后len++
+        num[len++] = sum % 10;
+        sum /= 10; //去除sum的末位
+    }
+    for(int k = len - 1; k >= 0; k--) { //从高位开始输出
+        printf("%d", num[k]);
+        if(k > 0 && k % 3 == 0)
+            printf(","); //每3位一个逗号，最后一位除外
     }
     return 0;
 }
