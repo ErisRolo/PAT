c898d59f3d2a980d28e3bd817e025ee78f340ea1
guohouxiao <122492508@qq.com>
2018-07-18 23:04:15 +0800

    update 1008-advanced

---
+++ PAT (Advanced Level) Practise/1008. Elevator (20).cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main() {
+    int n, total = 0, now = 0, to;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &to);
+        if(to > now) {
+            total += ((to - now) * 6);
+        } else {
+            total += ((now - to) * 4);
+        }
+        total += 5;
+        now = to;
+    }
+    printf("%d\n", total);
+    return 0;
+}
