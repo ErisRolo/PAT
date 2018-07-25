9d2b2390eeb94a6470aeb8d239d584f8b557ad22
guohouxiao <122492508@qq.com>
2018-07-25 21:27:37 +0800

    AC 1042-advanced

--- PAT (Advanced Level) Practise/1042. Shuffling Machine (20).cpp
+++ PAT (Advanced Level) Practise/1042. Shuffling Machine (20).cpp
@@ -1,73 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
+/**
+* 分析：因为题目直接给出了交换后的顺序，所以可以用两个数组start和end分别保存交换前后的顺序
+*       用一个order数组记录顺序，然后按重复次数赋值即可
+*       注意数组下标要使用1-54，方便和顺序一一对应，虽然0没有用但数组要开55个
+*       学习用字符数组输出数字前的字符的技巧，整除用来分组，取余用来输出数据
+*       注意输出前后数据要减1加1，使数据分到正确的分组，也避免整除时取余结果为0不能输出13
+*       实在理解不了就麻烦点分组判断输出，整除13的单独输出，但不要忘了输出符号
+*       结尾不能有空格，但不要用printf("%c", 8)，会判错，逻辑里加空格就好
+**/
 
-using namespace std;
-
-int N;
-
-int h[55];
-int tmp[55];
-int result[55];
+#include <cstdio>
+char c[5] = {'S', 'H', 'C', 'D', 'J'};
 
 int main() {
-    scanf("%d", &N);
-    for (int i = 1; i <= 54; i++) {
-        scanf("%d", &h[i]);
-        result[i] = i; // 初始化, 第 i 个放在 i 位置
+    int K, start[55], end[55], order[55];
+    scanf("%d", &K); //repeat times
+    for(int i = 1; i <= 54; i++)
+        start[i] = i;
+    for(int i = 1; i <= 54; i++) {
+        scanf("%d", &order[i]);
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 1; j <= 54; j++) {
-            int num = result[j]; // 枚举第 i 个数字
-            // h[j] 表示把 num 放到 h[j] 位置上去
-            tmp[h[j]] = num;
-        }
-        for( int j = 1; j <= 54; j++) {
-            result[j] = tmp[j];
-        }
+    for(int i = 0; i < K; i++) {
+        for(int j = 1; j <= 54; j++)
+            end[order[j]] = start[j];
+        for(int k = 1; k <= 54; k++)
+            start[k] = end[k]; //将排序后的结果作为下一次排序前的顺序
     }
-    // 1   2         13  14  .........26
-    // S1, S2, ..., S13, H1, H2, ..., H13, C1, C2, ..., C13, D1, D2, ..., D13, J1, J2
-    for (int i = 1; i <= 54; i++) {
-        int c = (result[i] - 1) / 13 + 1; // 第几组
-        if (i == 1) {
-
-            switch (c) {
-            case 1:
-                printf("S%d", result[i] - 13 * (c - 1));
-                break;
-            case 2:
-                printf("H%d", result[i] - 13 * (c - 1));
-                break;
-            case 3:
-                printf("C%d", result[i] - 13 * (c - 1));
-                break;
-            case 4:
-                printf("D%d", result[i] - 13 * (c - 1));
-                break;
-            case 5:
-                printf("J%d", result[i] - 13 * (c - 1));
-            }
-        } else {
-            switch (c) {
-            case 1:
-                printf(" S%d", result[i] - 13 * (c - 1));
-                break;
-            case 2:
-                printf(" H%d", result[i] - 13 * (c - 1));
-                break;
-            case 3:
-                printf(" C%d", result[i] - 13 * (c - 1));
-                break;
-            case 4:
-                printf(" D%d", result[i] - 13 * (c - 1));
-                break;
-            case 5:
-                printf(" J%d", result[i] - 13 * (c - 1));
-            }
-        }
+    for(int i = 1; i <= 54; i++) {
+        end[i]--; //因为字符数组下标是0-4，减1使整除数据分到正确的分组
+        printf("%c%d", c[end[i] / 13], end[i] % 13 + 1); //前面减1后面输出加上
+        if(i != 54)
+            printf(" ");
     }
+    //printf("%c", 8); //ASCII码为8的是backspace，用来删除末尾空格
     return 0;
 }
