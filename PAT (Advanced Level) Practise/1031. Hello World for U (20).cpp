f65bb7d7fae1d7168d9ad0b1b922093b1a380876
guohouxiao <122492508@qq.com>
2018-07-28 11:10:53 +0800

    AC 1031-advanced

--- PAT (Advanced Level) Practise/1031. Hello World for U (20).cpp
+++ PAT (Advanced Level) Practise/1031. Hello World for U (20).cpp
@@ -1,63 +1,32 @@
-/* 1031. Hello World for U (20)
+/**
+* 分析：打印图形，根据题目里的限制条件推公式即可
+*       然后套公式算三个位置的字符个数，根据个数打印
+*       如果不清楚怎么打印就先输出三个位置的数据看一下
+*       公式要写对，不要按自己的理解凑公式，按题目来
+**/
 
-Given any string of N (>=5) characters, you are asked to form the characters into the shape of U. For example, "helloworld" can be printed as:
-
-h  d
-e  l
-l  r
-lowo
-That is, the characters must be printed in the original order, starting top-down from the left vertical line with n1 characters, then left to right along the bottom line with n2 characters, and finally bottom-up along the vertical line with n3 characters. And more, we would like U to be as squared as possible -- that is, it must be satisfied that n1 = n3 = max { k| k <= n2 for all 3 <= n2 <= N} with n1 + n2 + n3 - 2 = N.
-Input Specification:
-
-Each input file contains one test case. Each case contains one string with no less than 5 and no more than 80 characters in a line. The string contains no white space.
-
-Output Specification:
-
-For each test case, print the input string in the shape of U as specified in the description.
-
-Sample Input:
-helloworld!
-Sample Output:
-h   !
-e   d
-l   l
-lowor */
+#include <cstdio>
+#include <cstring>
 #include <iostream>
-#include <string>
-
 using namespace std;
+const int maxn = 81;
 
-string s;
-
-char g[100][100];
-int main(){
-    cin >> s;
-    int len_v = (s.size()+2) / 3;
-    int narrow_h = s.size() - 2 * len_v;
-    int len_h = narrow_h+ 2;
-
-    int index = 0;
-    for(int i = 1; i <= len_v; i++){
-        for(int j = 1; j <= len_h; j++){
-            g[i][j] = ' ';
-        }
+int main() {
+    char str[maxn];
+    cin >> str;
+    int N = strlen(str);
+    int n1, n2, n3;
+    n1 = (N + 2) / 3;
+    n3 = n1;
+    n2 = N + 2 - n1 - n3;
+    //printf("%d %d %d", n1, n2, n3);
+    for(int i = 0; i < n1 - 1; i++) {
+        printf("%c", str[i]);
+        for(int j = 0; j < n2 - 2; j++)
+            printf(" ");
+        printf("%c\n", str[N - i - 1]);
     }
-    for(int i = 1; i <= len_v; i++){
-        g[i][1] = s[index++];
-    }
-    for(int i = 1; i <= narrow_h; i++){
-        g[len_v][i+1] = s[index++];
-    }
-    for(int i = len_v; i >= 1; i--){
-        g[i][len_h] = s[index++];
-    }
-
-    for(int i = 1; i <= len_v;i++){
-        for(int j = 1; j <= len_h; j++){
-            cout << g[i][j];
-        }
-        cout << endl;
-    }
-
+    for(int i = 0; i < n2; i++)
+        printf("%c", str[n1 + i - 1]);
     return 0;
 }
