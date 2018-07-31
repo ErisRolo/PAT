59bbd3bf9e0c202212f91b63182790437e381c27
guohouxiao <122492508@qq.com>
2018-07-31 22:33:48 +0800

    AC 1041-advanced

--- PAT (Advanced Level) Practise/1041. Be Unique (20).cpp
+++ PAT (Advanced Level) Practise/1041. Be Unique (20).cpp
@@ -1,47 +1,32 @@
-/* 1041. Be Unique (20)
-
-Being unique is so important to people on Mars that even their lottery is designed in a unique way. The rule of winning is simple: one bets on a number chosen from [1, 104]. The first one who bets on a unique number wins. For example, if there are 7 people betting on 5 31 5 88 67 88 17, then the second one who bets on 31 wins.
-
-Input Specification:
-
-Each input file contains one test case. Each case contains a line which begins with a positive integer N (<=105) and then followed by N bets. The numbers are separated by a space.
-
-Output Specification:
-
-For each test case, print the winning number in a line. If there is no winner, print "None" instead.
-
-Sample Input 1:
-7 5 31 5 88 67 88 17
-Sample Output 1:
-31
-Sample Input 2:
-5 888 666 666 888 888
-Sample Output 2:
-None */
-
+/**
+* 分析：输入一行数，找出第一个不同的数，用简单的散列（Hash）即可
+*       因为范围1<= d <=10^4，所以用一个10^4大小的整型数组存储数据出现次数即可
+*       再用一个整型数组存储输入数据，输出时按输入顺序判断那几个数据即可
+*       不用遍历整个hash，遍历整个hash也不能按顺序输出第一个不同的数
+*       简单题，主要想到用hash就行，这题我写的和晴神一样，可能是看到这是hash这一章的题受到提示了吧
+*       要记住使用hash的情景，用空间换时间，比如此题需要统计不同数字出现的次数
+*       记住hash的主要思想，就是将元素通过一个函数转换为整数，使得该整数可以尽量唯一地代表这个元素
+**/
 
 #include <cstdio>
-#include <vector>
-using namespace std;
+const int maxn = 10001;
 
-int N;
-int a[10001] = {0};
-
-vector<int> vec;
-int main(){
+int main() {
+    int N, bet;
     scanf("%d", &N);
-    int tmp;
-    for(int i = 0; i < N; i++){
-        scanf("%d", &tmp);
-        vec.push_back(tmp);
-        a[tmp]++;
+    int temp[N], hash[maxn];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &bet);
+        temp[i] = bet;
+        hash[bet]++;
     }
-    for(int i = 0; i < vec.size(); i++){
-        if(a[vec[i]] == 1){
-            printf("%d", vec[i]);
-            return 0;
+    for(int i = 0; i < N; i++) {
+        if(hash[temp[i]] == 1) {
+            printf("%d", temp[i]);
+            break;
         }
+        if(i == N - 1)
+            printf("None");
     }
-    printf("None");
     return 0;
 }
