9002953c04ccb66d1ac24f724003a234bfcf1d04
guohouxiao <122492508@qq.com>
2018-08-01 19:11:53 +0800

    AC 1036-advanced

--- PAT (Advanced Level) Practise/1036. Boys vs Girls (25).cpp
+++ PAT (Advanced Level) Practise/1036. Boys vs Girls (25).cpp
@@ -1,89 +1,51 @@
-/*  1036. Boys vs Girls (25)
-
-This time you are asked to tell the difference between the lowest grade of all the male students and the highest grade of all the female students.
-
-Input Specification:
-
-Each input file contains one test case. Each case contains a positive integer N, followed by N lines of student information. Each line contains a student's name, gender, ID and grade, separated by a space, where name and ID are strings of no more than 10 characters with no space, gender is either F (female) or M (male), and grade is an integer between 0 and 100. It is guaranteed that all the grades are distinct.
-
-Output Specification:
-
-For each test case, output in 3 lines. The first line gives the name and ID of the female student with the highest grade, and the second line gives that of the male student with the lowest grade. The third line gives the difference gradeF-gradeM. If one such kind of student is missing, output "Absent" in the corresponding line, and output "NA" in the third line instead.
-
-Sample Input 1:
-
-3
-Joe M Math990112 89
-Mike M CS991301 100
-Mary F EE990830 95
-
-Sample Output 1:
-Mary EE990830
-Joe Math990112
-6
-
-Sample Input 2:
-1
-Jean M AA980920 60
-Sample Output 2:
-Absent
-Jean AA980920
-NA
- */
+/**
+* 分析：要求输出成绩最高的女生的信息和成绩最低的男生的信息，以及他们的成绩差值
+*       首先将所有人按从女到男，成绩从高到低排序，则第一个就是成绩最高的女生，最后一个就是成绩最低的男生
+*       循环统计性别数量来分成三种情况：全女、全男、有男有女，按不同的情况输出即可
+*       此题注意输出格式，认真审题，要仔细！！！
+**/
 
 #include <cstdio>
-#include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
-struct Stu
-{
+struct student {
     char name[11];
-    char gender;
+    char gender; //F or M
     char id[11];
     int score;
 };
 
-vector<Stu> male;
-vector<Stu> female;
-
-int N;
-
-bool cmp_male(Stu &lhs, Stu &rhs){
-    if(lhs.score < rhs.score) return true;
-    else return false;
+//按从女到男，成绩从高到低的顺序排列
+bool cmp(student stu1, student stu2) {
+    if(stu1.gender != stu2.gender)
+        return stu1.gender < stu2.gender;
+    else if(stu1.score != stu2.score)
+        return stu1.score > stu2.score;
 }
-bool cmp_female(Stu &lhs, Stu &rhs){
-    if(lhs.score > rhs.score) return true;
-    else return false;
-}
-int main()
-{
+
+int main() {
+    int N, fName, fId, mName, mId;
+    int gender = 0;
     scanf("%d", &N);
-    for (int i = 0; i < N; i++)
-    {
-        Stu stu;
-        scanf("%s %c %s %d", stu.name, &stu.gender, stu.id, &stu.score);
-        if(stu.gender == 'M') male.push_back(stu);
-        else female.push_back(stu);
+    student stu[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%s %c %s %d", stu[i].name, &stu[i].gender, stu[i].id, &stu[i].score);
     }
-    if(female.size() == 0) printf("Absent\n");
-    else{
-        sort(female.begin(), female.end(), cmp_female);
-        printf("%s %s\n", female[0].name, female[0].id);
+    for(int i = 0; i < N; i++) {
+        if(stu[i].gender == 'F')
+            gender++;
     }
-    if(male.size() == 0) printf("Absent\n");
-    else{
-        sort(male.begin(), male.end(), cmp_male);
-        printf("%s %s\n", male[0].name, male[0].id);
-    }
-    if(male.size()== 0 || female.size() ==0){
-        printf("NA");
-    }
-    else{
-        int high = female[0].score;
-        int low = male[0].score;
-        printf("%d", high-low);
+    if(gender == N)
+        printf("%s %s\nAbsent\nNA", stu[0].name, stu[0].id); //全为女性
+    else if(gender == 0)
+        printf("Absent\n%s %s\nNA", stu[N - 1].name, stu[N - 1].id); //全为男性
+    else {
+        sort(stu, stu + N, cmp);
+        printf("%s %s\n%s %s\n%d", stu[0].name, stu[0].id,
+               stu[N - 1].name, stu[N - 1].id,
+               stu[0].score - stu[N - 1].score);
     }
     return 0;
 }
