9fa7c94a30f56206d0aff384efb9b9631532d552
guohouxiao <122492508@qq.com>
2018-08-08 18:38:48 +0800

    AC 1133-advanced

--- PAT (Advanced Level) Practise/1133. Splitting A Linked List.cpp
+++ PAT (Advanced Level) Practise/1133. Splitting A Linked List.cpp
@@ -1,36 +1,76 @@
+/**
+* 分析：用了32分钟，比柳神多一倍的代码量，只拿了23分，还有一个2分的点没拿到。。。
+*       分析原因可能是个别数据输出格式有误，不容易找出是哪里的数据
+*       然后将用来判断输出格式的三个bool变量，是否第一次输出负数、是否有负数、是否有小于K的数
+*       改为用一个bool变量控制输出，即是否第一次输出数据，成功A掉题目
+*       因为这三种情况的输出格式实质上都是与是不是第一次输出有关，无论是什么类型的数据，输出格式是一样的
+*       判断前面有什么数据反而复杂化了问题，既然输出格式一样，就没有必要分别讨论每种情况，用一种思维去分别判断三中情况就好
+*       还是多思考多积累多总结
+**/
+
 #include <cstdio>
-#include <vector>
+#include <algorithm>
 using namespace std;
-struct node {
-    int data, next;
-}list[100000];
-vector<int> v[3];
+const int maxn = 100010;
+
+struct Node {
+    int address, data, next;
+    int order;
+} node[maxn];
+
+bool cmp(Node a, Node b) {
+    if(a.order == -1 || b.order == -1)
+        return a.order > b.order;
+    else
+        return a.order < b.order;
+}
+
 int main() {
-    int start, n, k, a;
-    scanf("%d%d%d", &start, &n, &k);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a);
-        scanf("%d%d", &list[a].data, &list[a].next);
+    for(int i = 0; i < maxn; i++)
+        node[i].order = -1;
+    int begin, n, k;
+    int address, data, next;
+    scanf("%d %d %d", &begin, &n, &k);
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d %d", &address, &data, &next);
+        node[address].address = address;
+        node[address].data = data;
+        node[address].next = next;
     }
-    int p = start;
+    int p = begin, count = 0;
     while(p != -1) {
-        int data = list[p].data;
-        if (data < 0)
-            v[0].push_back(p);
-        else if (data >= 0 && data <= k)
-            v[1].push_back(p);
-        else
-            v[2].push_back(p);
-        p = list[p].next;
+        node[p].order = ++count;
+        p = node[p].next;
     }
-    int flag = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < v[i].size(); j++) {
-            if (flag == 0) {
-                printf("%05d %d ", v[i][j], list[v[i][j]].data);
-                flag = 1;
+    sort(node, node + maxn, cmp);
+    int isFirst = 1;
+    for(int i = 0; i < count; i++) {
+        if(node[i].data < 0) {
+            if(isFirst == 1) {
+                printf("%05d %d ", node[i].address, node[i].data);
+                isFirst = 0;
             } else {
-                printf("%05d\n%05d %d ", v[i][j], v[i][j], list[v[i][j]].data);
+                printf("%05d\n%05d %d ", node[i].address, node[i].address, node[i].data);
+            }
+        }
+    }
+    for(int i = 0; i < count; i++) {
+        if(node[i].data >= 0 && node[i].data <= k) {
+            if(isFirst == 1) {
+                printf("%05d %d ", node[i].address, node[i].data);
+                isFirst = 0;
+            } else {
+                printf("%05d\n%05d %d ", node[i].address, node[i].address, node[i].data);
+            }
+        }
+    }
+    for(int i = 0; i < count; i++) {
+        if(node[i].data > k) {
+            if(isFirst == 1) {
+                printf("%05d %d ", node[i].address, node[i].data);
+                isFirst = 0;
+            } else {
+                printf("%05d\n%05d %d ", node[i].address, node[i].address, node[i].data);
             }
         }
     }
