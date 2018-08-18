ca57179c3436ede92b69a27f488ff4fa7ba2bd7d
guohouxiao <122492508@qq.com>
2018-08-18 15:25:59 +0800

    AC 1055-advanced

--- PAT (Advanced Level) Practise/1055. The World's Richest (25).cpp
+++ PAT (Advanced Level) Practise/1055. The World's Richest (25).cpp
@@ -1,50 +1,49 @@
-/* 1055. The World's Richest (25)
+/**
+* 分析：排序简单题，十五分钟一次AC直接25分
+*       跪求考试全是这种难度的。。。
+**/
 
-Forbes magazine publishes every year its list of billionaires based on the annual ranking of the world's wealthiest people. Now you are supposed to simulate this job, but concentrate only on the people in a certain range of ages. That is, given the net worths of N people, you must find the M richest people in a given range of their ages.
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int maxn = 100010;
 
-Input Specification:
+struct rich {
+    char name[9];
+    int age;
+    int worth;
+} r[maxn];
 
-Each input file contains one test case. For each case, the first line contains 2 positive integers: N (<=105) - the total number of people, and K (<=103) - the number of queries. Then N lines follow, each contains the name (string of no more than 8 characters without space), age (integer in (0, 200]), and the net worth (integer in [-106, 106]) of a person. Finally there are K lines of queries, each contains three positive integers: M (<= 100) - the maximum number of outputs, and [Amin, Amax] which are the range of ages. All the numbers in a line are separated by a space.
+bool cmp(rich a, rich b) {
+    int s = strcmp(a.name, b.name);
+    if(a.worth != b.worth)
+        return a.worth > b.worth;
+    else if(a.age != b.age)
+        return a.age < b.age;
+    else if(s != 0)
+        return s < 0;
+}
 
-Output Specification:
-
-For each query, first print in a line "Case #X:" where X is the query number starting from 1. Then output the M richest people with their ages in the range [Amin, Amax]. Each person's information occupies a line, in the format
-
-Name Age Net_Worth
-The outputs must be in non-increasing order of the net worths. In case there are equal worths, it must be in non-decreasing order of the ages. If both worths and ages are the same, then the output must be in non-decreasing alphabetical order of the names. It is guaranteed that there is no two persons share all the same of the three pieces of information. In case no one is found, output "None".
-Sample Input:
-12 4
-Zoe_Bill 35 2333
-Bob_Volk 24 5888
-Anny_Cin 95 999999
-Williams 30 -22
-Cindy 76 76000
-Alice 18 88888
-Joe_Mike 32 3222
-Michael 5 300000
-Rosemary 40 5888
-Dobby 24 5888
-Billy 24 5888
-Nobody 5 0
-4 15 45
-4 30 35
-4 5 95
-1 45 50
-Sample Output:
-Case #1:
-Alice 18 88888
-Billy 24 5888
-Bob_Volk 24 5888
-Dobby 24 5888
-Case #2:
-Joe_Mike 32 3222
-Zoe_Bill 35 2333
-Williams 30 -22
-Case #3:
-Anny_Cin 95 999999
-Michael 5 300000
-Alice 18 88888
-Cindy 76 76000
-Case #4:
-None
- */
+int main() {
+    int n, k;
+    int num, min, max;
+    scanf("%d %d", &n, &k);
+    for(int i = 0; i < n; i++)
+        scanf("%s %d %d", r[i].name, &r[i].age, &r[i].worth);
+    sort(r, r + n, cmp);
+    for(int i = 1; i <= k; i++) {
+        scanf("%d %d %d", &num, &min, &max);
+        printf("Case #%d:\n", i);
+        int temp = num;
+        for(int j = 0; j < n && temp != 0; j++) {
+            if(r[j].age >= min && r[j].age <= max) {
+                printf("%s %d %d\n", r[j].name, r[j].age, r[j].worth);
+                temp--;
+            }
+        }
+        if(temp == num)
+            printf("None\n");
+    }
+    return 0;
+}
