8f1be04c658336b0e7f24ea6c85fbdd7c28259f5
guohouxiao <122492508@qq.com>
2018-08-20 00:59:10 +0800

    AC 1095-advanced

--- PAT (Advanced Level) Practise/1095. Cars on Campus (30).cpp
+++ PAT (Advanced Level) Practise/1095. Cars on Campus (30).cpp
@@ -1,76 +1,90 @@
+/**
+* 分析：应该是最难的排序题了，参考了之后写了好久，还是有一个3分的测试点没有通过，运行超时
+*       分析原因是计算停车数量的时候处理方式不是很好，每次都是重新开始重复计算
+*       按晴神的写法，没有必要每次重新计算，因为时间点是从小到大排序的
+*       在查了一次之后，下一次的查找可以在上一次的结果之上继续进行，可以避免超时
+*       这题很棒，能学到的很多，具体总结到文档里
+**/
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdio>
 #include <cstring>
-#include <string>
 #include <map>
+#include <string>
+#include <algorithm>
 using namespace std;
-struct node {
-    char id[10];
+const int maxn = 10010;
+const int maxk = 8 * 10010;
+
+struct record {
+    char id[8];
     int time;
-    int flag = 0;
-};
-bool cmp1(node a, node b) {
-    if(strcmp(a.id, b.id) != 0)
+    int flag = 0; //in=1,out=-1
+} rec[maxn], nrec[maxn];
+
+char temp[4];
+map<string, int> ptime;
+
+bool cmp1(record a, record b) {
+    if(strcmp(a.id, b.id))
         return strcmp(a.id, b.id) < 0;
     else
         return a.time < b.time;
 }
-bool cmp2(node a, node b) {
+
+bool cmp2(record a, record b) {
     return a.time < b.time;
 }
+
 int main() {
-    int n, k, maxtime = -1, tempindex = 0;
-    scanf("%d%d\n", &n, &k);
-    vector<node> record(n), car;
+    int n, k;
+    int h, m, s, time, maxtime = 0;
+    int num = 0;
+    scanf("%d%d", &n, &k);
     for(int i = 0; i < n; i++) {
-        char temp[5];
-        int h, m, s;
-        scanf("%s %d:%d:%d %s\n", record[i].id, &h, &m, &s, temp);
-        int temptime = h * 3600 + m * 60 + s;
-        record[i].time = temptime;
-        record[i].flag = strcmp(temp, "in") == 0 ? 1 : -1;
+        scanf("%s %d:%d:%d %s", rec[i].id, &h, &m, &s, temp);
+        rec[i].time = h * 3600 + m * 60 + s;
+        if(strcmp(temp, "in") == 0)
+            rec[i].flag = 1;
+        else
+            rec[i].flag = -1;
     }
-    sort(record.begin(), record.end(), cmp1);
-    map<string, int> mapp;
-    for(int i = 0; i < n - 1; i++) {
-        if(strcmp(record[i].id, record[i+1].id) == 0 && record[i].flag == 1 && record[i+1].flag == -1) {
-            car.push_back(record[i]);
-            car.push_back(record[i+1]);
-            mapp[record[i].id] += (record[i+1].time - record[i].time);
-            if(maxtime < mapp[record[i].id]) {
-                maxtime = mapp[record[i].id];
-            }
+    sort(rec, rec + n, cmp1);
+    for(int i = 0; i < n; i++) {
+        if(strcmp(rec[i].id, rec[i + 1].id) == 0 && rec[i].flag == 1 && rec[i + 1].flag == -1) {
+            nrec[num++] = rec[i];
+            nrec[num++] = rec[i + 1];
+            ptime[rec[i].id] += rec[i + 1].time - rec[i].time;
+            if(maxtime < ptime[rec[i].id])
+                maxtime = ptime[rec[i].id];
         }
     }
-    sort(car.begin(), car.end(), cmp2);
-    vector<int> cnt(n);
-    for(int i = 0; i < car.size(); i++) {
-        if(i == 0)
-            cnt[i] += car[i].flag;
-         else
-            cnt[i] = cnt[i - 1] + car[i].flag;
-    }
+    sort(nrec, nrec + num, cmp2);
+    int count = 0, now = 0;
     for(int i = 0; i < k; i++) {
-        int h, m, s;
         scanf("%d:%d:%d", &h, &m, &s);
-        int temptime = h * 3600 + m * 60 + s;
-        int j;
-        for(j = tempindex; j < car.size(); j++) {
-            if(car[j].time > temptime) {
-                printf("%d\n", cnt[j - 1]);
-                break;
-            } else if(j == car.size() - 1) {
-                printf("%d\n", cnt[j]);
-            }
+        time = h * 3600 + m * 60 + s;
+//        int count = 0;
+//        for(int j = 0; j < num; j++) {
+//            if(nrec[j].time <= time) {
+//                if(nrec[j].flag == 1)
+//                    count++;
+//                else
+//                    count--;
+//            }
+//        }
+        while(now < num && nrec[now].time <= time) {
+            if(nrec[now].flag == 1)
+                count++;
+            else
+                count--;
+            now++;
         }
-        tempindex = j;
+        printf("%d\n", count);
     }
-    for(map<string, int>::iterator it = mapp.begin(); it != mapp.end(); it++) {
+    for(map<string, int>::iterator it = ptime.begin(); it != ptime.end(); it++) {
         if(it->second == maxtime)
             printf("%s ", it->first.c_str());
     }
-    printf("%02d:%02d:%02d", maxtime / 3600, (maxtime % 3600) / 60, maxtime % 60);
+    printf("%02d:%02d:%02d", maxtime / 3600, maxtime % 3600 / 60, maxtime % 60);
     return 0;
 }
