26bd73a31f72c29f7cfebdb763c08887ebc9320d
guohouxiao <122492508@qq.com>
2018-08-24 01:25:31 +0800

    AC 1102-advanced

--- PAT (Advanced Level) Practise/1102. Invert a Binary Tree (25).cpp
+++ PAT (Advanced Level) Practise/1102. Invert a Binary Tree (25).cpp
@@ -1,103 +1,82 @@
-/* 1102. Invert a Binary Tree (25)
+/**
+* 分析：反转二叉树并进行层次遍历和中序遍历
+*       用了不到半个小时AC，仍然用的建树的方法
+*       看了下柳神的做法，虽然她没有建树但是这次代码量和我差不多，说明一直用建树的方法是可行的
+*       多加练习不断熟练提高编码速度即可
+*       说一下这道题，因为题目给的是结点编号，所以用静态实现比较方便
+*       反转就是输入左右子树的时候交换，根结点是没有被指向过的结点
+*       这道题的坑点在于输入的时候要吸收换行符，注意这个细节学会这个技巧
+**/
 
-
-The following is from Max Howell @twitter:
-
-Google: 90% of our engineers use the software you wrote (Homebrew), but you can't invert a binary tree on a whiteboard so fuck off.
-
-Now it's your turn to prove that YOU CAN invert a binary tree!
-
-Input Specification:
-
-Each input file contains one test case. For each case, the first line gives a positive integer N (<=10) which is the total number of nodes in the tree -- and hence the nodes are numbered from 0 to N-1. Then N lines follow, each corresponds to a node from 0 to N-1, and gives the indices of the left and right children of the node. If the child does not exist, a "-" will be put at the position. Any pair of children are separated by a space.
-
-Output Specification:
-
-For each test case, print in the first line the level-order, and then in the second line the in-order traversal sequences of the inverted tree. There must be exactly one space between any adjacent numbers, and no extra space at the end of the line.
-
-Sample Input:
-8
-1 -
-- -
-0 -
-2 7
-- -
-- -
-5 -
-4 6
-Sample Output:
-3 7 2 6 4 0 5 1
-6 5 7 4 3 2 0 1
- */
-
-#include <iostream>
 #include <cstdio>
-#include <vector>
-#include <algorithm>
 #include <queue>
-
 using namespace std;
-int N;
+const int maxn = 11;
 
-struct Node{
-    int left;
-    int right;
-}list[20];
+int n, root, cnt1 = 0, cnt2 = 0;
+char a, b;
 
-bool vis[20] = {false}; 
+struct Node {
+    int lchild;
+    int rchild;
+    int flag; //用来标记找出根结点，默认应该是为0，不行手动赋值为0
+} node[maxn];
 
-void level(int root){
+void BFS(int root) {
     queue<int> q;
     q.push(root);
-    int n = 0;
-    while(!q.empty()){
-        int now = q.front();
+    while(!q.empty()) {
+        int temp = q.front();
         q.pop();
-        if(n == 0) printf("%d", now);
-        else printf(" %d", now);
-        n++;
-        if(list[now].right!=-1) q.push(list[now].right);
-        if(list[now].left!=-1) q.push(list[now].left);
+        if(temp != -1) {
+            printf("%d", temp);
+            cnt1++;
+            if(cnt1 < n)
+                printf(" ");
+        }
+        if(node[temp].lchild != -1)
+            q.push(node[temp].lchild);
+        if(node[temp].rchild != -1)
+            q.push(node[temp].rchild);
     }
-    printf("\n");
 }
 
-bool flag = true;
-void in(int root){
-    if(root == -1) return;
-    in(list[root].right);
-    if(flag){
-        printf("%d", root);
-        flag = false;
-    } 
-    else printf(" %d", root);
-    in(list[root].left);
+void inOrder(int root) {
+    if(root == -1)
+        return;
+    inOrder(node[root].lchild);
+    printf("%d", root);
+    cnt2++;
+    if(cnt2 < n)
+        printf(" ");
+    inOrder(node[root].rchild);
 }
-int main(){
-    scanf("%d", &N);
-    for(int i = 0; i < N; i++){
-        char a, b;
-        cin >> a >> b;
-        if(a == '-') list[i].left = -1;
-        else{
-            list[i].left = a - '0';
-            vis[list[i].left] = true;
-        } 
-        if(b == '-') list[i].right = -1;
-        else{
-            list[i].right = b - '0';
-            vis[list[i].right] = true;
-        } 
+
+int main() {
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("\n%c %c", &a, &b);
+        if(a == '-')
+            node[i].rchild = -1;
+        else {
+            node[i].rchild = a - '0';
+            node[node[i].rchild].flag = 1;
+        }
+        if(b == '-')
+            node[i].lchild = -1;
+        else {
+            node[i].lchild = b - '0';
+            node[node[i].lchild].flag = 1;
+        }
     }
-    int root = -1;
-    for(int i = 0; i < N; i++){
-        if(!vis[i]){
+    for(int i = 0; i < n; i++) {
+        if(node[i].flag == 0) {
             root = i;
             break;
         }
     }
-    level(root);
-    in(root);
+    BFS(root);
     printf("\n");
+    inOrder(root);
     return 0;
 }
