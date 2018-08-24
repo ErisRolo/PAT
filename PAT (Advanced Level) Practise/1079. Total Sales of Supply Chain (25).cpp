764e378d2e36b1f3b4507ba9760d7974e5dcefd6
guohouxiao <122492508@qq.com>
2018-08-24 23:17:27 +0800

    AC 1079-advanced

--- PAT (Advanced Level) Practise/1079. Total Sales of Supply Chain (25).cpp
+++ PAT (Advanced Level) Practise/1079. Total Sales of Supply Chain (25).cpp
@@ -1,38 +1,55 @@
-#include <vector>
-#include <cmath>
-using namespace std;
-struct node {
-    double data;
-    vector<int> child;
-};
-vector<node> v;
-double ans = 0.0, p, r;
+/**
+* 分析：树的遍历+DFS，勉勉强强40分钟一遍AC，还是对DFS不太熟练
+*       要仔细想清楚递归参数和边界，不过思路还算清晰
+*       注意变量类型都是double不是int，让这坑了耽误了一会时间
+**/
 
-void dfs(int index, int depth) {
-    if(v[index].child.size() == 0) {
-        ans += v[index].data * pow(1 + r, depth);
-        return ;
+#include <cstdio>
+#include <vector>
+using namespace std;
+const int maxn = 100010;
+
+int n;
+double p, r;
+double np = p;
+double sale = 0;
+
+struct Node {
+    int weight;
+    int flag; //标记是否为零售商
+    vector<int> child;
+} node[maxn];
+
+void DFS(int index,  double np) {
+    if(node[index].flag == 1) {
+        sale += node[index].weight * np;
+        np = np / (1.0 + r / 100);
+        return;
     }
-    for(int i = 0; i < v[index].child.size(); i++)
-        dfs(v[index].child[i], depth + 1);
+    if(node[index].flag == 0) {
+        for(int i = 0; i < node[index].child.size(); i++)
+            DFS(node[index].child[i], np + np * r / 100);
+    }
 }
+
 int main() {
-    int n, k, c;
     scanf("%d %lf %lf", &n, &p, &r);
-    r = r / 100;
-    v.resize(n);
     for(int i = 0; i < n; i++) {
-        scanf("%d", &k);
-        if(k == 0) {
-            scanf("%lf", &v[i].data);
+        int cnt, id, amount;
+        scanf("%d", &cnt);
+        if(cnt == 0) {
+            scanf("%d", &amount);
+            node[i].flag = 1;
+            node[i].weight = amount;
         } else {
-            for(int j = 0; j < k; j++) {
-                scanf("%d", &c);
-                v[i].child.push_back(c);
+            node[i].flag = 0;
+            for(int j = 0; j < cnt; j++) {
+                scanf("%d", &id);
+                node[i].child.push_back(id);
             }
         }
     }
-    dfs(0, 0);
-    printf("%.1f", ans * p);
+    DFS(0, p);
+    printf("%.1f", sale);
     return 0;
 }
