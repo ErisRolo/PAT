67d6dbe22833b2fa4c905767c4e577598a4e8daf
guohouxiao <122492508@qq.com>
2018-08-25 00:48:26 +0800

    AC 1094-advanced

--- PAT (Advanced Level) Practise/1094. The Largest Generation (25).cpp
+++ PAT (Advanced Level) Practise/1094. The Largest Generation (25).cpp
@@ -1,61 +1,51 @@
-C++
+/**
+* 分析：树的遍历+BFS，有点晚有点困，15分钟一次AC
+*       考试的时候倒数第二题是这种难度就好了
+**/
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
 #include <cstdio>
 #include <vector>
+#include <queue>
 using namespace std;
-vector<int> v[100];
-int book[100];
-void dfs(int index, int level) {
-    book[level]++;
-    for(int i = 0; i < v[index].size(); i++)
-        dfs(v[index][i], level+1);
-}
-int main() {
-    int n, m, a, k, c;
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d",&a, &k);
-        for(int j = 0; j < k; j++) {
-            scanf("%d", &c);
-            v[a].push_back(c);
+const int maxn = 101;
+
+int n, m, levels = 1, maxnum = 0, maxlevel = 1;
+int level[maxn], num[maxn]; //结点所在的层数，该层具有的结点数
+vector<int> child[maxn];
+
+void BFS() {
+    queue<int> q;
+    q.push(1);
+    num[levels] = 1;
+    while(!q.empty()) {
+        int parent = q.front();
+        q.pop();
+        if(child[parent].size() != 0) {
+            levels++;
+            for(int i = 0; i < child[parent].size(); i++) {
+                q.push(child[parent][i]);
+                level[child[parent][i]] = level[parent] + 1;
+                num[level[parent] + 1]++;
+            }
         }
     }
-    dfs(1, 1);
-    int maxnum = 0, maxlevel = 1;
-    for(int i = 1; i < 100; i++) {
-        if(book[i] > maxnum) {
-            maxnum = book[i];
+}
+
+int main() {
+    int id, k, kid;
+    scanf("%d%d", &n, &m);
+    for(int i = 0; i < m; i++) {
+        scanf("%d%d", &id, &k);
+        for(int j = 0; j < k; j++) {
+            scanf("%d", &kid);
+            child[id].push_back(kid);
+        }
+    }
+    level[1] = 1;
+    BFS();
+    for(int i = 1; i <= levels; i++) {
+        if(num[i] > maxnum) {
+            maxnum = num[i];
             maxlevel = i;
         }
     }
