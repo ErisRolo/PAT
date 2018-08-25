8621f3b92131997d518b8bdda1f6e7cebe149726
guohouxiao <122492508@qq.com>
2018-08-26 01:24:22 +0800

    AC 1099-advanced

--- PAT (Advanced Level) Practise/1099. Build A Binary Search Tree (30).cpp
+++ PAT (Advanced Level) Practise/1099. Build A Binary Search Tree (30).cpp
@@ -1,45 +1,60 @@
+/**
+* 分析：BST的题做到第三题终于十分钟一次AC了！！！30分！！！
+*       给了结点序号的用静态实现真踏马好写！
+*       一定要十分熟悉BST的性质！！！
+**/
+
 #include <cstdio>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
-struct TREE {
-    int left, right, value;
-};
-vector<TREE> tree;
-vector<int> v;
-int t = 0;
-void inorder(int root) {
-    if(tree[root].left == -1 && tree[root].right == -1) {
-        tree[root].value = v[t++];
-        return ;
+const int maxn = 110;
+
+int n, left, right, index = 0, cnt = 0;
+int data[maxn];
+
+struct Node {
+    int data;
+    int lchild, rchild;
+} node[maxn];
+
+void inOrder(int root) {
+    if(root == -1)
+        return;
+    inOrder(node[root].lchild);
+    node[root].data = data[index++];
+    inOrder(node[root].rchild);
+}
+
+void BFS(int root) {
+    queue<int> q;
+    q.push(root);
+    while(!q.empty()) {
+        int temp = q.front();
+        cnt++;
+        if(cnt < n)
+            printf("%d ", node[temp].data);
+        else
+            printf("%d", node[temp].data);
+        q.pop();
+        if(node[temp].lchild != -1)
+            q.push(node[temp].lchild);
+        if(node[temp].rchild != -1)
+            q.push(node[temp].rchild);
     }
-    if(tree[root].left != -1) inorder(tree[root].left);
-    tree[root].value = v[t++];
-    if(tree[root].right != -1) inorder(tree[root].right);
 }
 
 int main() {
-    int n;
     scanf("%d", &n);
-    tree.resize(n);
-    v.resize(n);
-    for(int i = 0; i < n; i++)
-        scanf("%d %d",&tree[i].left, &tree[i].right);
-    for(int i = 0; i < n; i++)
-        scanf("%d", &v[i]);
-    sort(v.begin(), v.end());
-    inorder(0);
-    queue<int> q;
-    q.push(0);
-    printf("%d", tree[0].value);
-    while(!q.empty()) {
-        int node = q.front();
-        q.pop();
-        if(node != 0) printf(" %d", tree[node].value);
-        if(tree[node].left != -1) q.push(tree[node].left);
-        if(tree[node].right != -1) q.push(tree[node].right);
+    for(int i = 0; i < n; i++) {
+        scanf("%d%d", &left, &right);
+        node[i].lchild = left;
+        node[i].rchild = right;
     }
+    for(int i = 0; i < n; i++)
+        scanf("%d", &data[i]);
+    sort(data, data + n);
+    inOrder(0);
+    BFS(0);
     return 0;
 }
