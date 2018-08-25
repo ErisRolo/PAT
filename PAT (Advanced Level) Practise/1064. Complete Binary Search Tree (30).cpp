cae6ff42598c83ab6a931586f01a4c928aa4e55f
guohouxiao <122492508@qq.com>
2018-08-26 00:59:43 +0800

    AC 1064-advanced

--- PAT (Advanced Level) Practise/1064. Complete Binary Search Tree (30).cpp
+++ PAT (Advanced Level) Practise/1064. Complete Binary Search Tree (30).cpp
@@ -1,31 +1,32 @@
+/**
+* 分析：建一棵完美二叉搜索树，要求熟悉CBT和BST的性质
+*       总结下性质记住即可
+**/
+
 #include <cstdio>
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
-vector<int> in, level;
-void levelorder(int start, int end, int index) {
-    if(start > end) return ;
-    int n = end - start + 1;
-    int l = log(n + 1) / log(2); // 除了最后一层的层数
-    int leave = n - (pow(2, l) - 1);// 最后一层的叶子节点数
-    int root = start + (pow(2, l - 1) - 1) + min((int)pow(2, l - 1), leave); // pow(2, l - 1) - 1是除了root结点所在层和最后一层外，左子树的结点个数，pow(2, l - 1) 是l+1层最多拥有的属于根结点左子树的结点个数，min(pow(2, l - 1), leave)是最后一个结点真正拥有的属于根结点左子树上的结点个数
-    level[index] = in[root];
-    levelorder(start, root - 1, 2 * index + 1);
-    levelorder(root + 1, end, 2 * index + 2);
+const int maxn = 1010;
+
+int n, CBT[maxn], data[maxn], index = 0;
+
+void inOrder(int root) {
+    if(root > n)
+        return;
+    inOrder(2 * root);
+    CBT[root] = data[index++];
+    inOrder(2 * root + 1);
 }
+
 int main() {
-    int n;
     scanf("%d", &n);
-    in.resize(n);
-    level.resize(n);
-    for(int i = 0 ; i < n; i++)
-        scanf("%d", &in[i]);
-    sort(in.begin(), in.end());
-    levelorder(0, n - 1, 0);
-    printf("%d", level[0]);
-    for(int i = 1; i < n; i++)
-        printf(" %d", level[i]);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &data[i]);
+    }
+    sort(data, data + n);
+    inOrder(1);
+    for(int i = 1; i <= n - 1; i++)
+        printf("%d ", CBT[i]);
+    printf("%d", CBT[n]);
     return 0;
 }
