3fa7d4f41dfb42d21054928b3f67c8719ca5530a
guohouxiao <122492508@qq.com>
2018-08-26 22:11:59 +0800

    AC 1107-advanced

--- PAT (Advanced Level) Practise/1107. Social Clusters (30).cpp
+++ PAT (Advanced Level) Practise/1107. Social Clusters (30).cpp
@@ -1,97 +1,77 @@
-/* 1107. Social Clusters (30)
+/**
+* 分析：并查集，有点难理解，不要弄混父亲结点和根结点，合并的时候是根结点
+*       模板固定，真正需要自己思考编写的是并查集的建立部分
+*       再多刷几道题理解一下吧
+**/
 
-When register on a social network, you are always asked to specify your hobbies in order to find some potential friends with the same hobbies. A "social cluster" is a set of people who have some of their hobbies in common. You are supposed to find all the clusters.
-
-Input Specification:
-
-Each input file contains one test case. For each test case, the first line contains a positive integer N (<=1000), the total number of people in a social network. Hence the people are numbered from 1 to N. Then N lines follow, each gives the hobby list of a person in the format:
-
-Ki: hi[1] hi[2] ... hi[Ki]
-
-where Ki (>0) is the number of hobbies, and hi[j] is the index of the j-th hobby, which is an integer in [1, 1000].
-
-Output Specification:
-
-For each case, print in one line the total number of clusters in the network. Then in the second line, print the numbers of people in the clusters in non-increasing order. The numbers must be separated by exactly one space, and there must be no extra space at the end of the line.
-
-Sample Input:
-8
-3: 2 7 10
-1: 4
-2: 5 3
-1: 4
-1: 3
-1: 4
-4: 6 8 1 5
-1: 4
-Sample Output:
-3
-4 3 1 */
 #include <cstdio>
 #include <algorithm>
-
 using namespace std;
+const int maxn = 1010;
 
-const int N = 1010;
-int father[N]; 
-int isRoot[N] = {0}; 
-int course[N] = {0};
+int n, k, t, cnt = 0;
+int father[maxn];
+int isRoot[maxn];
+int hobby[maxn];
 
-int findFather(int x){
-    int a = x;
-    while(x != father[x]) x = father[x];
-
-    while(a != father[a]){
-        int z = a;
-        a = father[a];
-        father[z] = x;
-    }
-    return x;
-}
-
-void Union(int a, int b){
-    int faA = findFather(a);
-    int faB = findFather(b);
-    if(faA != faB) father[faA] = faB;
-}
-
-void init(int n){
-    for(int i = 1; i <=n; i++){
+void init(int n) {
+    for(int i = 1; i <= n; i++) {
         father[i] = i;
-        isRoot[i] = false;
+        isRoot[i] = 0;
     }
 }
-bool cmp(int a, int b){
+
+int findFather(int a) {
+    int b = a;
+    while(a != father[a]) {
+        a = father[a];
+    }
+    //路径压缩
+    while(b != father[b]) {
+        int c = b;
+        father[b] = a;
+        b = father[c];
+    }
+    return a;
+}
+
+void Union(int a, int b) {
+    int fa = findFather(a);
+    int fb = findFather(b);
+    if(fa != fb)
+        father[fb] = fa;
+}
+
+bool cmp(int a, int b) {
     return a > b;
 }
-int main(){
-    int n, k ,h;
+
+int main() {
     scanf("%d", &n);
     init(n);
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++) {
         scanf("%d:", &k);
-        for(int j = 0; j < k; j++){
-            scanf("%d", &h);
-            if(course[h] == 0){
-                course[h] = i;
-            }
-            Union(i, findFather(course[h]));
+        for(int j = 0; j < k; j++) {
+            scanf("%d", &t);
+            if(hobby[t] == 0)
+                hobby[t] = i; //令第一个对兴趣t感兴趣的人i为兴趣t的集合的根结点
+            Union(i, hobby[t]);
         }
     }
-    for(int i = 1; i <=n; i++){
+    for(int i = 1; i <= n; i++)
         isRoot[findFather(i)]++;
-    }
-    int ans = 0;
-    for(int i = 1; i <=n; i++){
-        if(isRoot[i] != 0){
-            ans++;
+    for(int i = 1; i <= n; i++) {
+        if(isRoot[i] != 0) {
+            cnt++;
         }
     }
-    printf("%d\n", ans);
-    sort(isRoot+1, isRoot+n+1, cmp);
-    for(int i = 1; i <= ans; i++){
-        printf("%d",isRoot[i]);
-        if(i < ans) printf(" ");
+    printf("%d\n",cnt);
+    sort(isRoot + 1, isRoot + 1 + n, cmp);
+    for(int i = 1; i <= cnt; i++) {
+        if(i < cnt)
+            printf("%d ", isRoot[i]);
+        else
+            printf("%d", isRoot[i]);
     }
     return 0;
 }
