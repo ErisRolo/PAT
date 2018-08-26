d26c5c13265996f8a6bbc5adf585a065ede29d42
guohouxiao <122492508@qq.com>
2018-08-27 03:33:56 +0800

    udpate 1098-advanced

---
+++ PAT (Advanced Level) Practise/1098. Insertion or Heap Sort (25).cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+    int n,m,c1,c2;
+    scanf("%d %d %d %d",&n,&m,&c1,&c2);
+    for(int i=0;i<n;i++){
+
+    }
+    for(int i=0;i<m;i++){
+        scanf("%d %d %d",);
+    }
+}
