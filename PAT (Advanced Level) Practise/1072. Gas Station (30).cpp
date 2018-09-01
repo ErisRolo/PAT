bf16541a266e9929819015056c0c274673b44f07
guohouxiao <122492508@qq.com>
2018-09-01 23:44:39 +0800

    AC 1072-advanced

--- PAT (Advanced Level) Practise/1072. Gas Station (30).cpp
+++ PAT (Advanced Level) Practise/1072. Gas Station (30).cpp
@@ -1,78 +1,105 @@
+/**
+* 分析：太难了太难了。。。
+*       不要把所有看上去不是直接问最短路径的题当成Dijkstra+DFS题，比如这道，是纯Dijkstra题
+*       模板仍一样，主要是在主函数进行逻辑处理，要深刻理解最短路径的思想才可以
+*       然后把算法求出来的最短路径当工具，去结合题目解决实际问题
+*       因为CB不支持C++11，因此stoi不能用，学会手动循环实现字符串和数字的转换
+**/
+
+#include <cstdio>
+#include <string>
 #include <iostream>
 #include <algorithm>
-#include <string>
 using namespace std;
-const int inf = 999999999;
-int n, m, k, ds, station;
-int e[1020][1020], dis[1020];
-bool visit[1020];
-int main() {
-    fill(e[0], e[0] + 1020 * 1020, inf);
-    fill(dis, dis + 1020, inf);
-    scanf("%d%d%d%d", &n, &m, &k, &ds);
-    for(int i = 0; i < k; i++) {
-        int tempdis;
-        string s, t;
-        cin >> s >> t >> tempdis;
-        int a, b;
-        if(s[0] == 'G') {
-            s = s.substr(1);
-            a = n + stoi(s);
-        } else {
-            a = stoi(s);
-        }
-        if(t[0] == 'G') {
-            t = t.substr(1);
-            b = n + stoi(t);
-        } else {
-            b = stoi(t);
-        }
-        e[a][b] = e[b][a] = tempdis;
+const int maxn = 1020;
+const int inf = 1000000000;
+
+int n, m, k, ds;
+int g[maxn][maxn];
+int d[maxn];
+bool vis[maxn];
+
+int getnum(string s) {
+    int ans = 0;
+    if (s[0] == 'G') {
+        for (int i = 1; i < s.length(); i++)
+            ans = ans * 10 + s[i] - '0';
+        ans += n;
+    } else {
+        for (int i = 0; i < s.length(); i++)
+            ans = ans * 10 + s[i] - '0';
     }
-    int ansid = -1;
-    double ansdis = -1, ansaver = inf;
-    for(int index = n + 1; index <= n + m; index++) {
-        double mindis = inf, aver = 0;
-        fill(dis, dis + 1020, inf);
-        fill(visit, visit + 1020, false);
-        dis[index] = 0;
-        for(int i = 0; i < n + m; i++) {
-            int u = -1, minn = inf;
-            for(int j = 1; j <= n + m; j++) {
-                if(visit[j] == false && dis[j] < minn) {
-                    u = j;
-                    minn = dis[j];
+    return ans;
+}
+
+void Dijkstra(int s) {
+    fill(vis, vis + maxn, false);
+    fill(d, d + maxn, inf);
+    d[s] = 0;
+    for(int i = 1; i <= n + m; i++) {
+        int u = -1, min = inf;
+        for(int j = 1; j <= n + m; j++) {
+            if(vis[j] == false && d[j] < min) {
+                u = j;
+                min = d[j];
+            }
+        }
+        if(u == -1)
+            return;
+        vis[u] = true;
+        for(int v = 1; v <= n + m; v++) {
+            if(vis[v] == false && g[u][v] != inf) {
+                if(d[u] + g[u][v] < d[v]) {
+                    d[v] = d[u] + g[u][v];
                 }
             }
-            if(u == -1) break;
-            visit[u] = true;
-            for(int v = 1; v <= n + m; v++) {
-                if(visit[v] == false && dis[v] > dis[u] + e[u][v])
-                    dis[v] = dis[u] + e[u][v];
-            }
         }
-        for(int i = 1; i <= n; i++) {
-            if(dis[i] > ds) {
+    }
+}
+
+int main() {
+    fill(g[0], g[0] + maxn * maxn, inf);
+    scanf("%d%d%d%d", &n, &m, &k, &ds);
+    string p1, p2;
+    int a, b, dist;
+    for(int i = 0; i < k; i++) {
+        cin >> p1 >> p2 >> dist;
+        a = getnum(p1);
+        b = getnum(p2);
+        g[a][b] = g[b][a] = dist;
+    }
+    int ansid = -1;
+    double ansdis = -1, ansavg = inf;
+    for(int i = n + 1; i <= n + m; i++) {
+        double mindis = inf, avg = 0;
+        Dijkstra(i);
+        for(int j = 1; j <= n; j++) {
+            if(d[j] > ds) {
                 mindis = -1;
                 break;
             }
-            if(dis[i] < mindis) mindis = dis[i];
-            aver += 1.0 * dis[i];
+            if(d[j] < mindis) {
+                mindis = d[j];
+            }
+            avg += 1.0 * d[j];
         }
-        if(mindis == -1) continue;
-        aver = aver / n;
+        avg = avg / n;
+        if(mindis == -1)
+            continue;
         if(mindis > ansdis) {
-            ansid = index;
+            ansid = i;
             ansdis = mindis;
-            ansaver = aver;
-        } else if(mindis == ansdis && aver < ansaver) {
-            ansid = index;
-            ansaver = aver;
-        }
+            ansavg = avg;
+        } else if(mindis == ansdis && avg < ansavg) {
+            ansid = i;
+            ansavg = avg;
+        } //因为是从小到大遍历，所以无需再判断更小的ID
     }
     if(ansid == -1)
         printf("No Solution");
-    else
-        printf("G%d\n%.1f %.1f", ansid - n, ansdis, ansaver);
+    else {
+        printf("G%d\n", ansid - n);
+        printf("%.1f %.1f", ansdis, ansavg);
+    }
     return 0;
 }
