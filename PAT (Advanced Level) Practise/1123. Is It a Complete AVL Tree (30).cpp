8f8d8626c83fbb86ab919a35bbf93030061d00d7
guohouxiao <122492508@qq.com>
2018-09-04 22:55:04 +0800

    AC 1123-advanced

--- PAT (Advanced Level) Practise/1123. Is It a Complete AVL Tree (30).cpp
+++ PAT (Advanced Level) Practise/1123. Is It a Complete AVL Tree (30).cpp
@@ -1,206 +1,141 @@
-/* 1123. Is It a Complete AVL Tree (30)
-
-An AVL tree is a self-balancing binary search tree. In an AVL tree, the heights of the two child subtrees of any node differ by at most one; if at any time they differ by more than one, rebalancing is done to restore this property. Figures 1-4 illustrate the rotation rules.
-
-    
-    
-Now given a sequence of insertions, you are supposed to output the level-order traversal sequence of the resulting AVL tree, and to tell if it is a complete binary tree.
-
-Input Specification:
-
-Each input file contains one test case. For each case, the first line contains a positive integer N (<= 20). Then N distinct integer keys are given in the next line. All the numbers in a line are separated by a space.
-
-Output Specification:
-
-For each test case, insert the keys one by one into an initially empty AVL tree. Then first print in a line the level-order traversal sequence of the resulting AVL tree. All the numbers in a line must be separated by a space, and there must be no extra space at the end of the line. Then in the next line, print "YES" if the tree is complete, or "NO" if not.
-
-Sample Input 1:
-5
-88 70 61 63 65
-Sample Output 1:
-70 63 88 61 65
-YES
-Sample Input 2:
-8
-88 70 61 96 120 90 65 68
-Sample Output 2:
-88 65 96 61 70 90 120 68
-NO */
+/**
+* 分析：判断一棵AVL是不是完全二叉树，要求输出层序遍历
+*       AVL部分直接写模板即可
+*       判断完全二叉树，则根据层序遍历，第一个叶子结点后出现的结点都是叶子结点，否则就不是完全二叉树
+**/
 
 #include <cstdio>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
+const int maxn = 30;
 
+int n, data, root = -1;
 
-int N;
+struct Node {
+    int data;
+    int lchild, rchild;
+    int height;
+} node[maxn];
 
-struct Node{
-	int data;
-	int height;
-	Node *left;
-	Node *right;
-};
-
-vector<Node *> vec; // 保存层序结果
-
-// 主要是为了更新高度
-Node *newNode(int data){
-	Node *node = new Node;
-	node->data = data;
-	node->height = 1;
-	node->left = node->right = NULL;
-	return node;
+int getHeight(int root) {
+    if(root == -1)
+        return 0;
+    else
+        return node[root].height;
 }
 
-int getHeight(Node *root){
-	if(root == NULL) return 0;
-	return root->height;
+int getBalanceFactor(int root) {
+    return getHeight(node[root].lchild) - getHeight(node[root].rchild);
 }
 
-void update(Node *root){
-	root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
+void updateHeight(int root) {
+    node[root].height = max(getHeight(node[root].lchild), getHeight(node[root].rchild)) + 1;
 }
 
-int balance(Node *root){
-	return getHeight(root->left) - getHeight(root->right);
+void L(int &root) {
+    int temp = node[root].rchild;
+    node[root].rchild = node[temp].lchild;
+    node[temp].lchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
 }
 
-// 适合 RR 型， 左旋
-//    1
-//   /  \
-//  1    1
-//      / \
-//     1   1
-//          \
-//          (X)
-void L(Node *&root){
-	Node *tmp = root->right;
-	root->right = tmp->left;
-	tmp->left = root;
-	update(root); // 更新高度
-	update(tmp);
-	root = tmp;
+void R(int &root) {
+    int temp = node[root].lchild;
+    node[root].lchild = node[temp].rchild;
+    node[temp].rchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
 }
 
-// 适合 LL 型， 左旋
-//       1
-//      / \ 
-//     1   1 
-//    / \
-//   1   1
-//  /
-// (X)
-void R(Node *&root){
-	Node *tmp = root->left;
-	root->left = tmp->right;
-	tmp->right = root;
-	update(root); // 更新高度
-	update(tmp);
-	root = tmp;
+int index = 0;
+void insert(int &root, int x) {
+    if(root == -1) {
+        node[index].data = x;
+        node[index].height = 1;
+        node[index].lchild = node[index].rchild = -1;
+        root = index++;
+        return;
+    }
+    if(x < node[root].data) {
+        insert(node[root].lchild, x);
+        updateHeight(root);
+        if(getBalanceFactor(root) == 2) {
+            if(getBalanceFactor(node[root].lchild) == 1) {
+                R(root);
+            } else if (getBalanceFactor(node[root].lchild) == -1) {
+                L(node[root].lchild);
+                R(root);
+            }
+        }
+    } else  {
+        insert(node[root].rchild, x);
+        updateHeight(root);
+        if(getBalanceFactor(root) == -2) {
+            if(getBalanceFactor(node[root].rchild) == -1) {
+                L(root);
+            } else if (getBalanceFactor(node[root].rchild) == 1) {
+                R(node[root].rchild);
+                L(root);
+            }
+        }
+    }
 }
 
-
-
-
-void insert(Node *&root, int data){
-	if(root==NULL){
-		root = newNode(data);
-	}
-	else{
-		if(data < root->data){
-			insert(root->left, data);
-			update(root);
-			if(balance(root) == 2){
-				if(balance(root->left) == 1){
-					// LL 型， 右旋 root
-					R(root);
-				}
-				else if(balance(root->left) == -1){
-					// LR 型， 先左旋 left， 再右旋 root
-					L(root->left); // 这里 L 就是左旋
-					R(root);       
-				}
-			}
-		}
-		else if(data > root->data){
-			insert(root->right, data);
-			update(root);
-			if(balance(root) == -2){
-				if(balance(root->right) == -1){
-					// RR 型， 左旋 root
-					L(root);
-				}
-				else if(balance(root->right) == 1){
-					// RL 型， 先右旋 right， 再左旋 root
-					R(root->right); 
-					L(root);       
-				}
-			}
-		}
-	}
-
+bool isleaf(int root) {
+    if(2 * (node[root].lchild + 1) > n)
+        return true;
+    else
+        return false;
 }
 
-void levelTraverse(Node *root){
-	queue<Node *> q;
-	q.push(root);
-	bool flag = true;
-	vec.push_back(NULL);
-	while(!q.empty()){
-		Node *now = q.front();
-		if(flag){
-			flag = false;
-			printf("%d", now->data);
-		}
-		else{
-			printf(" %d", now->data);
-		}
-		vec.push_back(now);
-		q.pop();
-		if(now->left!=NULL) q.push(now->left);
-		if(now->right!=NULL) q.push(now->right);
-	}
-	printf("\n");
+int cnt = 0;
+bool haschild, iscompelete = true;
+void BFS(int root) {
+    queue<int> q;
+    q.push(root);
+    if(node[root].lchild != -1 || node[root].rchild != -1)
+        haschild = true;
+    else
+        haschild = false;
+    while(!q.empty()) {
+        int temp = q.front();
+        q.pop();
+        cnt++;
+        if(cnt < n)
+            printf("%d ", node[temp].data);
+        else
+            printf("%d\n", node[temp].data);
+        if(node[temp].lchild != -1) {
+            if(haschild == false)
+                iscompelete = false;
+            q.push(node[temp].lchild);
+        } else {
+            haschild = false;
+        }
+        if(node[temp].rchild != -1) {
+            if(haschild == false)
+                iscompelete = false;
+            q.push(node[temp].rchild);
+        } else {
+            haschild = false;
+        }
+    }
 }
 
-void isComplete(Node *root){
-	bool flag = true;
-	// vec 按编号保存 node
-	// 1 2 3 4 5 ... 10 ... 
-	for(int i = N; i > 1; i--){
-		int parent_index = i / 2;
-		if(i % 2){ // 奇数， 则是右节点
-			if(vec[parent_index]->right != vec[i]){
-				flag = false;
-				break;
-			}
-		}
-		else{
-			if(vec[parent_index]->left != vec[i]){
-				flag = false;
-				break;
-			}
-		}
-	}
-	if(flag) printf("YES\n");
-	else printf("NO\n");
-}
-
-
-
-int main(){
-	scanf("%d", &N);
-	int tmp;
-	Node *root = NULL;
-	for(int i = 0; i < N; i++){
-		scanf("%d", &tmp);
-		insert(root, tmp);
-	}
-	levelTraverse(root);
-	isComplete(root);
-	return 0;
+int main() {
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &data);
+        insert(root, data);
+    }
+    BFS(root);
+    if(iscompelete)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
 }
