3b03feb709f33b0cc0aa859e8c83157b43f9a1e0
guohouxiao <122492508@qq.com>
2018-09-05 21:07:20 +0800

    update 1147-advanced

---
+++ PAT (Advanced Level) Practise/1147. Heaps (30).cpp
@@ -0,0 +1,56 @@
+/**
+* 分析：判断是不是一个堆，是大顶堆还是小顶堆，并输出后序遍历
+*       利用完全二叉树的性质和堆的定义判断，后序遍历直接写即可
+**/
+
+#include <cstdio>
+const int maxn = 1010;
+
+int m, n;
+int cbt[maxn];
+int flag;
+
+void postorder(int index) {
+    if(index > n)
+        return;
+    postorder(index * 2);
+    postorder(index * 2 + 1);
+    if(index != 1)
+        printf("%d ", cbt[index]);
+    else
+        printf("%d\n", cbt[index]);
+}
+
+int main() {
+    scanf("%d%d", &m, &n);
+    for(int i = 0; i < m; i++) {
+        for(int j = 1; j <= n; j++) {
+            scanf("%d", &cbt[j]);
+        }
+        if(cbt[1] >= cbt[2]) {
+            flag = 1;
+            for(int k = 1; k <= n / 2; k++) {
+                if(cbt[k] < cbt[2 * k] || ( 2 * k + 1 <= n && cbt[k] < cbt[2 * k + 1])) {
+                    flag = 0;
+                }
+            }
+        }
+        if(cbt[1] <= cbt[2]) {
+            flag = -1;
+            for(int k = 1; k <= n / 2; k++) {
+                if(cbt[k] > cbt[2 * k] || ( 2 * k + 1 <= n && cbt[k] > cbt[2 * k + 1])) {
+                    flag = 0;
+                }
+            }
+        }
+        if(flag == 1) {
+            printf("Max Heap\n");
+        } else if(flag == -1) {
+            printf("Min Heap\n");
+        } else if(flag == 0) {
+            printf("Not Heap\n");
+        }
+        postorder(1);
+    }
+    return 0;
+}
