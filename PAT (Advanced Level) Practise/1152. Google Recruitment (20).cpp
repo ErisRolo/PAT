638c4b6d2f32a83bf71730e2757907113b0eaae0
guohouxiao <122492508@qq.com>
2019-02-02 11:44:33 +0800

    AC 1152-advanced

--- PAT (Advanced Level) Practise/1152. Google Recruitment (20).cpp
+++ PAT (Advanced Level) Practise/1152. Google Recruitment (20).cpp
@@ -1,31 +1,35 @@
+/**
+* 分析：素数判断、字符串转整型atoi(char[])
+**/
+
 #include <cstdio>
 #include <cstdlib>
 #include <string>
 #include <iostream>
 using namespace std;
 
-bool isprime(int n){
-	for(int i=2;i*i<=n;i++)
-		if(n%i==0)
-			return false;
-	return true;	
+bool isprime(int n) {
+    for(int i = 2; i * i <= n; i++)
+        if(n % i == 0)
+            return false;
+    return true;
 }
 
-int main(){
-	int l,k;
-	string s,r;
-	bool f = false;
-	scanf("%d%d",&l,&k);
-	cin>>s;
-	for(int i=0;i<=l-k;i++){
-		r=s.substr(i,k);
-		if(isprime(atoi(r.c_str()))){
-			cout<<r;
-			f=true;
-			break;
-		}
-	}
-	if(f==false)
-		printf("404\n");
-	return 0;
-} 
+int main() {
+    int l, k;
+    string s, r;
+    bool f = false;
+    scanf("%d%d", &l, &k);
+    cin >> s;
+    for(int i = 0; i <= l - k; i++) {
+        r = s.substr(i, k);
+        if(isprime(atoi(r.c_str()))) {
+            cout << r;
+            f = true;
+            break;
+        }
+    }
+    if(f == false)
+        printf("404\n");
+    return 0;
+}
