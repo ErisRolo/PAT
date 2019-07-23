8686b4c0603c36fdb38bf1503a92976beae23be1
guohouxiao <122492508@qq.com>
2019-07-23 08:46:39 +0800

    AC 1073-advanced

--- PAT (Advanced Level) Practise/1073. Scientific Notation (20).cpp
+++ PAT (Advanced Level) Practise/1073. Scientific Notation (20).cpp
@@ -1,35 +1,48 @@
+/**
+* 分析：首先输出正负号，然后找到E的位置pos，同时根据此算出小数位的个数n
+*       接着将原字符串分为底数和指数两部分，同时将底数字符串去掉小数点，将指数字符串转为数字exp
+*       然后根据E后面的符号正负来分别输出
+*       若为-，则输出为0.0···0XXXX，先输出0.，然后输出exp-1个0，最后输出去掉小数点的底数字符串
+*       去掉小数点的底数字符串可用substr和s[0]拼接获得
+*       若为+，则主要判断小数点的位置，根据n和exp的关系输出
+*       这种题可以按照题意多写几组数据观察得出规律
+**/
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string s;
-    string num, ep;
-    char ns, es;
-    int e;
-    cin >> s;
-    e = s.find("E");
-    num = s.substr(1, e - 1);
-    ns = s[0];
-    es = s[e + 1];
-    ep = s.substr(e + 2);
-    num.erase(1, 1);
-    if(ns == '-')
-        cout << ns;
-    if(es == '-') {
+    string str, s1, s2;
+    int pos, exp, n; //E的位置 指数 小数位个数
+    cin >> str;
+    if(str[0] == '-')
+        cout << '-';
+    pos = str.find("E");
+    n = pos - 3;
+    int len = pos - 1;
+    s1 = str.substr(1, len);
+    s1 = s1[0] + s1.substr(2, s1.size() - 2);
+    s2 = str.substr(pos + 2, str.size() - pos - 2);
+    exp = atoi(s2.c_str());
+    if(str[pos + 1] == '-') {
         cout << "0.";
-        for(int i = 1; i < stoi(ep); i++) {
+        for(int i = 0; i < exp - 1; i++)
             cout << "0";
-        }
-        cout << num;
+        cout << s1;
     } else {
-        if(stoi(ep) >= num.length() - 1) {
-            cout << num;
-            for(int i = 0; i < stoi(ep) - num.length() + 1; i++) {
+        if(exp == n) {
+            cout << s1;
+        }
+        if(exp > n) {
+            cout << s1;
+            for(int i = 0; i < exp - n; i++)
                 cout << "0";
-            }
-        } else {
-            num.insert(stoi(ep) + 1, ".");
-            cout << num;
+        }
+        if(exp < n) {
+            for(int i = 0; i <= exp; i++)
+                cout << s1[i];
+            cout << ".";
+            for(int i = exp + 1; i < s1.size(); i++)
+                cout << s1[i];
         }
     }
     return 0;
