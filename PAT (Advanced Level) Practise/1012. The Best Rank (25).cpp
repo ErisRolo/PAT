e3ae7198110d03a7996e70f02c412d863b9a0240
guohouxiao <122492508@qq.com>
2019-07-24 11:07:21 +0800

    new 1012-advanced

--- PAT (Advanced Level) Practise/1012. The Best Rank (25).cpp
+++ PAT (Advanced Level) Practise/1012. The Best Rank (25).cpp
@@ -1,104 +1,99 @@
 /**
-* åˆ†æžï¼šæŽ’åºï¼Œå¤„ç†é€»è¾‘è¿‡äºŽå¤æ‚ï¼Œè€Œä¸”åªå¾—äº†22åˆ†ï¼Œæœ‰ä¸€ä¸ªæ®µé”™è¯¯ï¼Œä¸å¤ªå¥½æ‰¾ä¸æ‰¾äº†
-*       å‚è€ƒæŸ³ç¥žå’Œæ™´ç¥žå®å…¸çš„è§£æ³•ï¼Œå­¦ä¼šæ•°ç»„åœ¨æŽ’åºä¸­çš„ä½¿ç”¨ï¼Œé€»è¾‘ä¸å¤ªå¥½æŽŒæ¡çš„è¯ä¹Ÿè¦å­¦ä¼šç®€åŒ–ä»£ç 
+* ·ÖÎö£ºÅÅÐò£¬´¦ÀíÂß¼­¹ýÓÚ¸´ÔÓ£¬¶øÇÒÖ»µÃÁË22·Ö£¬ÓÐÒ»¸ö¶Î´íÎó£¬²»Ì«ºÃÕÒ²»ÕÒÁË
+*       ²Î¿¼ÁøÉñºÍÇçÉñ±¦µäµÄ½â·¨£¬Ñ§»áÊý×éÔÚÅÅÐòÖÐµÄÊ¹ÓÃ£¬Âß¼­²»Ì«ºÃÕÆÎÕµÄ»°Ò²ÒªÑ§»á¼ò»¯´úÂë
+*       ¸üÐÂ£ºÈ¥µôcmpÖÐµÄif(a!=b)£¬¿É½â¾ö¶Î´íÎó£¬²»ÖªµÀÎªÉ¶£¬ÎÊÏÂÇçÉñ
 **/
 
-#include <cstdio>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 const int maxn = 2010;
 
 struct student {
-    int id;
-    int c, m, e, a;
-    int rc, rm, re, ra;
+	int id;
+	int c, m, e, a;
+	int rc, rm, re, ra;
 } stu[maxn];
 
 bool cmpc(student a, student b) {
-    if(a.c != b.c)
-        return a.c > b.c;
+	return a.c > b.c;
 }
 
 bool cmpm(student a, student b) {
-    if(a.m != b.m)
-        return a.m > b.m;
+	return a.m > b.m;
 }
 
 bool cmpe(student a, student b) {
-    if(a.e != b.e)
-        return a.e > b.e;
+	return a.e > b.e;
 }
 
 bool cmpa(student a, student b) {
-    if(a.a != b.a)
-        return a.a > b.a;
+	return a.a > b.a;
 }
 
 int main() {
-    int n, m;
-    int query, flag = -1;
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < n; i++) {
-        scanf("%d %d %d %d", &stu[i].id, &stu[i].c, &stu[i].m, &stu[i].e);
-        stu[i].a = (stu[i].c + stu[i].m + stu[i].e) / 3.0 + 0.5;
-    }
-    sort(stu, stu + n, cmpc);
-    stu[0].rc = 1;
-    for(int i = 1; i < n; i++) {
-        if(stu[i].c != stu[i - 1].c)
-            stu[i].rc = i + 1;
-        else
-            stu[i].rc = stu[i - 1].rc;
-    }
-    sort(stu, stu + n, cmpm);
-    stu[0].rm = 1;
-    for(int i = 1; i < n; i++) {
-        if(stu[i].m != stu[i - 1].m)
-            stu[i].rm = i + 1;
-        else
-            stu[i].rm = stu[i - 1].rm;
-    }
-    sort(stu, stu + n, cmpe);
-    stu[0].re = 1;
-    for(int i = 1; i < n; i++) {
-        if(stu[i].e != stu[i - 1].e)
-            stu[i].re = i + 1;
-        else
-            stu[i].re = stu[i - 1].re;
-    }
-    sort(stu, stu + n, cmpa);
-    stu[0].ra = 1;
-    for(int i = 1; i < n; i++) {
-        if(stu[i].a != stu[i - 1].a)
-            stu[i].ra = i + 1;
-        else
-            stu[i].ra = stu[i - 1].ra;
-    }
-    while(m--) {
-        scanf("%d", &query);
-        for(int i = 0; i < n; i++) {
-            if(stu[i].id == query) {
-                flag = i;
-                break;
-            }
-        }
-        if(flag != -1) {
-            if(stu[flag].ra <= stu[flag].rc
-                    && stu[flag].ra <= stu[flag].rm
-                    && stu[flag].ra <= stu[flag].re)
-                printf("%d A\n", stu[flag].ra);
-            else if(stu[flag].rc <= stu[flag].rm
-                    && stu[flag].rc <= stu[flag].re)
-                printf("%d C\n", stu[flag].rc);
-            else if(stu[flag].rm <= stu[flag].re)
-                printf("%d M\n", stu[flag].rm);
-            else
-                printf("%d E\n", stu[flag].re);
-            flag = -1;
-        } else {
-            printf("N/A\n");
-        }
-    }
-    return 0;
+	int n, m;
+	int query, flag = -1;
+	scanf("%d %d", &n, &m);
+	for(int i = 0; i < n; i++) {
+		scanf("%d %d %d %d", &stu[i].id, &stu[i].c, &stu[i].m, &stu[i].e);
+		stu[i].a = (stu[i].c + stu[i].m + stu[i].e) / 3.0 + 0.5;
+	}
+	sort(stu, stu + n, cmpc);
+	stu[0].rc = 1;
+	for(int i = 1; i < n; i++) {
+		if(stu[i].c != stu[i - 1].c)
+			stu[i].rc = i + 1;
+		else
+			stu[i].rc = stu[i - 1].rc;
+	}
+	sort(stu, stu + n, cmpm);
+	stu[0].rm = 1;
+	for(int i = 1; i < n; i++) {
+		if(stu[i].m != stu[i - 1].m)
+			stu[i].rm = i + 1;
+		else
+			stu[i].rm = stu[i - 1].rm;
+	}
+	sort(stu, stu + n, cmpe);
+	stu[0].re = 1;
+	for(int i = 1; i < n; i++) {
+		if(stu[i].e != stu[i - 1].e)
+			stu[i].re = i + 1;
+		else
+			stu[i].re = stu[i - 1].re;
+	}
+	sort(stu, stu + n, cmpa);
+	stu[0].ra = 1;
+	for(int i = 1; i < n; i++) {
+		if(stu[i].a != stu[i - 1].a)
+			stu[i].ra = i + 1;
+		else
+			stu[i].ra = stu[i - 1].ra;
+	}
+	while(m--) {
+		scanf("%d", &query);
+		for(int i = 0; i < n; i++) {
+			if(stu[i].id == query) {
+				flag = i;
+				break;
+			}
+		}
+		if(flag != -1) {
+			if(stu[flag].ra <= stu[flag].rc
+			        && stu[flag].ra <= stu[flag].rm
+			        && stu[flag].ra <= stu[flag].re)
+				printf("%d A\n", stu[flag].ra);
+			else if(stu[flag].rc <= stu[flag].rm
+			        && stu[flag].rc <= stu[flag].re)
+				printf("%d C\n", stu[flag].rc);
+			else if(stu[flag].rm <= stu[flag].re)
+				printf("%d M\n", stu[flag].rm);
+			else
+				printf("%d E\n", stu[flag].re);
+			flag = -1;
+		} else {
+			printf("N/A\n");
+		}
+	}
+	return 0;
 }
-
