1a39c20054134dd5ec4acf50505955761e9932de
guohouxiao <122492508@qq.com>
2019-07-24 16:32:18 +0800

    new 1016-advanced

--- PAT (Advanced Level) Practise/1016. Phone Bills (25).cpp
+++ PAT (Advanced Level) Practise/1016. Phone Bills (25).cpp
@@ -1,61 +1,83 @@
 /**
-* åˆ†æï¼šæœ‰ç‚¹éº»çƒ¦ï¼Œå…¶ä¸­ç­›é€‰æœ‰æ•ˆæ•°æ®è¦å­¦ä¼šï¼Œå…¶ä»–çš„æ›´å¤šçš„æ˜¯è€ƒé€»è¾‘ï¼Œé™„ä¸ŠæŸ³ç¥ä»£ç ï¼Œæš‚æ—¶æˆ˜ç•¥æ€§æ”¾å¼ƒ
+* ·ÖÎö£ºµÚÒ»´Î¶ÀÁ¢Ğ´ÏÂÀ´£¬Ì«ÂıÁË£¬Ò»¸öĞ¡Ê±ÁË¡£¡£¡£ 
+*       Ö÷ÒªÕÆÎÕÊ±¼äÍ³¼ÆµÄ·½·¨ 
 **/
 
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-struct node {
-    string name;
-    int status, month, time, day, hour, minute;
-};
-bool cmp(node a, node b) {
-    return a.name != b.name ? a.name < b.name : a.time < b.time;
+const int maxn = 1010;
+
+struct Record {
+	char name[21];
+	int month,day,hour,minute;
+	int flag; // 1:on-line 2:off-line
+} rec[maxn];
+
+bool cmp(Record r1,Record r2) {
+	if(strcmp(r1.name,r2.name)!=0)
+		return strcmp(r1.name,r2.name)<0;
+	else if(r1.month!=r2.month)
+		return r1.month<r2.month;
+	else if(r1.day!=r2.day)
+		return r1.day<r2.day;
+	else if(r1.hour!=r2.hour)
+		return r1.hour<r2.hour;
+	else
+		return r1.minute<r2.minute;
+
 }
-double billFromZero(node call, int *rate) {
-    double total = rate[call.hour] * call.minute + rate[24] * 60 * call.day;
-    for (int i = 0; i < call.hour; i++)
-        total += rate[i] * 60;
-    return total / 100.0;
-}
+
+int toll[24];
+
 int main() {
-    int rate[25] = {0}, n;
-    for (int i = 0; i < 24; i++) {
-        scanf("%d", &rate[i]);
-        rate[24] += rate[i];
-    }
-    scanf("%d", &n);
-    vector<node> data(n);
-    for (int i = 0; i < n; i++) {
-        cin >> data[i].name;
-        scanf("%d:%d:%d:%d", &data[i].month, &data[i].day, &data[i].hour, &data[i].minute);
-        string temp;
-        cin >> temp;
-        data[i].status = (temp == "on-line") ? 1 : 0;
-        data[i].time = data[i].day * 24 * 60 + data[i].hour * 60 + data[i].minute;
-    }
-    sort(data.begin(), data.end(), cmp);
-    map<string, vector<node> > custom;
-    for (int i = 1; i < n; i++) {
-        if (data[i].name == data[i - 1].name && data[i - 1].status == 1 && data[i].status == 0) {
-            custom[data[i - 1].name].push_back(data[i - 1]);
-            custom[data[i].name].push_back(data[i]);
-        }
-    }
-    for (auto it : custom) {
-        vector<node> temp = it.second;
-        cout << it.first;
-        printf(" %02d\n", temp[0].month);
-        double total = 0.0;
-        for (int i = 1; i < temp.size(); i += 2) {
-            double t = billFromZero(temp[i], rate) - billFromZero(temp[i - 1], rate);
-            printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", temp[i - 1].day, temp[i - 1].hour, temp[i - 1].minute, temp[i].day, temp[i].hour, temp[i].minute, temp[i].time - temp[i - 1].time, t);
-            total += t;
-        }
-        printf("Total amount: $%.2f\n", total);
-    }
-    return 0;
+	for(int i=0; i<24; i++) {
+		scanf("%d",&toll[i]);
+	}
+	int n;
+	char word[9];
+	bool first = true;
+	double total=0;
+	scanf("%d",&n);
+	for(int i=0; i<n; i++) {
+		scanf("%s %d:%d:%d:%d %s",rec[i].name,&rec[i].month,&rec[i].day,&rec[i].hour,&rec[i].minute,word);
+		if(strcmp(word,"on-line")==0)
+			rec[i].flag=1;
+		else if(strcmp(word,"off-line")==0)
+			rec[i].flag=2;
+	}
+	sort(rec,rec+n,cmp);
+	for(int i=0; i<n; i++) {
+		if(strcmp(rec[i].name,rec[i+1].name)==0 && rec[i].flag==1 && rec[i+1].flag==2) {
+			if(first) {
+				printf("%s %02d\n",rec[i].name,rec[i].month);
+				first = false;
+			}
+			Record temp = rec[i];
+			int time=0;
+			double money=0;
+			while(temp.day<rec[i+1].day || temp.hour<rec[i+1].hour || temp.minute<rec[i+1].minute) {
+				temp.minute++;
+				time++;
+				money+=toll[temp.hour];
+				if(temp.minute==60) {
+					temp.minute=0;
+					temp.hour++;
+				}
+				if(temp.hour==24) {
+					temp.hour=0;
+					temp.day++;
+				}
+			}
+			money/=100.0;
+			total+=money;
+			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n",rec[i].day,rec[i].hour,rec[i].minute,rec[i+1].day,rec[i+1].hour,rec[i+1].minute,time,money);
+		}
+		if(strcmp(rec[i].name,rec[i+1].name)!=0 && total!=0) {
+			printf("Total amount: $%.2f\n",total);
+			first=true;
+			total=0;
+		}
+
+	}
+	return 0;
 }
