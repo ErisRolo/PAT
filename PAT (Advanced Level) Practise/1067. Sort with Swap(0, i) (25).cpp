65ebbb443255234416e76891b08bd9a2d9b474fe
guohouxiao <122492508@qq.com>
2019-07-28 21:09:20 +0800

    AC 1067-advanced

--- PAT (Advanced Level) Practise/1067. Sort with Swap(0, i) (25).cpp
+++ PAT (Advanced Level) Practise/1067. Sort with Swap(0, i) (25).cpp
@@ -1,3 +1,10 @@
+/**
+* 分析：思路正确，实现不对，原算法还有两个TLE只有19分
+*      参考晴神标程重新写
+*      学会用数组存位置的思想，一定要记得用，还有用预处理变量控制循环条件，以及swap()的使用
+*      循环找不在本位数时，适当剪枝，不然会有两个测试点TLE，估计我原答案也是那里的问题 
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 100010;
@@ -5,34 +12,79 @@ const int maxn = 100010;
 int pos[maxn];
 
 int main() {
-    int n, ans = 0;
-    scanf("%d", &n);
-    int left = n - 1, num;
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &num);
-        pos[num] = i;
-        if(num == i && num != 0) {
-            left--;
-        }
-    }
-    int k = 1;
-    while(left > 0) {
-        if(pos[0] == 0) {
-            while(k < n) {
-                if(pos[k] != k) {
-                    swap(pos[0], pos[k]);
-                    ans++;
-                    break;
-                }
-                k++;
-            }
-        }
-        while(pos[0] != 0) {
-            swap(pos[0], pos[pos[0]]);
-            ans++;
-            left--;
-        }
-    }
-    printf("%d\n", ans);
-    return 0;
+	int n,temp,left=0,cnt=0;
+	scanf("%d",&n);
+	for(int i=0; i<n; i++) {
+		scanf("%d",&temp);
+		pos[temp]=i;
+		if(pos[temp]!=temp && temp!=0)
+			left++;
+	}
+	int k=1; //不然会超时
+	while(left>0) {
+		if(pos[0]==0) {
+			while(k<n) {
+				if(pos[i]!=i) {
+					swap(pos[0],pos[i]);
+					cnt++;
+					break;
+				}
+				k++;
+			}
+		}
+		swap(pos[0],pos[pos[0]]);
+		cnt++;
+		left--;
+	}
+	printf("%d\n",cnt);
+	return 0;
 }
+
+
+
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//const int maxn = 100010;
+//
+//int d[maxn];
+//
+//int main() {
+//	int n,pos0,post,left=0,cnt=0;
+//	scanf("%d",&n);
+//	for(int i=0; i<n; i++) {
+//		scanf("%d",&d[i]);
+//		if(d[i]==0)
+//			pos0=i; //记录0的位置
+//		if(d[i]!=i && d[i]!=0)
+//			left++;
+//	}
+//	while(left>0) {
+//		if(pos0==0) {
+//			for(int i=0; i<n; i++) {
+//				if(d[i]!=i) {
+//					d[0]=d[i];
+//					d[i]=0;
+//					pos0=i;
+//					cnt++;
+//					break;
+//				}
+//			}
+//		}
+//		if(d[pos0]!=pos0) {
+//			for(int i=0; i<n; i++) {
+//				if(d[i]==pos0) {
+//					post=i;
+//				}
+//			}
+//			d[pos0]=pos0;
+//			d[post]=0;
+//			pos0=post;
+//			cnt++;
+//			left--;
+//		}
+//	}
+//	printf("%d\n",cnt);
+//	return 0;
+//}
