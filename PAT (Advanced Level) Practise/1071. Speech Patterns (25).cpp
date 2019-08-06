bca7ae50f23aae740bf4bdae37ba72e5388ddc36
guohouxiao <122492508@qq.com>
2019-08-06 16:59:18 +0800

    new 1071-advanced

--- PAT (Advanced Level) Practise/1071. Speech Patterns (25).cpp
+++ PAT (Advanced Level) Practise/1071. Speech Patterns (25).cpp
@@ -1,39 +1,53 @@
 /**
-* 分析：主要是注意一点，res为空时map也会统计其次数
-*       出现连续的无效字符时，res[]的次数就会增加
-*       因此需要加一个判断，当前单词非空时才统计次数
-*       还要注意对字符串末尾进行特判，到了末尾即使是有效数字也要将当前的res统计进map
-*       最后就是学习cctype文件下isalnum的使用，可以节省大量代码量
+* 分析：注意isalnum()的使用，判断是否为数字或字母，还有记得大写要转成小写，tolower函数的使用
+*       一开始写的O(n^2)的算法不好，还有两个WA，是思路有问题，直接遍历一遍就好
+*       新算法的坑点在于，每次临时字符串清空后，遇到的下一个字符仍是非法字符，则会统计一个空字符串的次数
+*       因此只有单词非空时才统计次数
 **/
 
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <cctype>
+#include <bits/stdc++.h>
 using namespace std;
 
+map<string, int> mp;
+
 int main() {
-    string str, res;
-    map<string, int> mp;
-    getline(cin, str);
-    for(int i = 0; i < str.size(); i++) {
-        if(isalnum(str[i])) {
-            str[i] = tolower(str[i]);
-            res += str[i];
+    int max = 0;
+    string s, res;
+    getline(cin, s);
+
+    for(int i = 0; i < s.size(); i++) {
+        if(isalnum(s[i])) {
+            s[i] = tolower(s[i]);
+            res += s[i];
         }
-        if(!isalnum(str[i]) || i == str.size() - 1) {
-            if(res.size() != 0)
+        if(!isalnum(s[i]) || i == s.size() - 1) {
+            if(res != "")
                 mp[res]++;
-            res = "";
+            res = ""; //这里导致的单词可能为空
         }
     }
-    int maxn = 0;
-    for(map<string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
-        if(it->second > maxn) {
-            maxn = it->second;
+
+//	for(int i=0; i<s.size(); i++) {
+//		if(isalnum(s[i])) {
+//			if(s[i]>='A'&&s[i]<='Z')
+//				s[i]=tolower(s[i]); //先大写转小写
+//			for(int j=i+1; j<s.size(); j++) {
+//				if(!isalnum(s[j])) {
+//					string temp = s.substr(i,j-i);
+//					mp[temp]++;
+//					i=j;
+//					break;
+//				}
+//			}
+//		}
+//	}
+
+    for(auto it = mp.begin(); it != mp.end(); it++) {
+        if(it->second > max) {
+            max = it->second;
             res = it->first;
         }
     }
-    cout << res << " " << maxn << endl;
+    cout << res << " " << max << endl;
     return 0;
 }
