73f120252ef7d984bac480f174626f0e8e6a3d27
guohouxiao <122492508@qq.com>
2019-08-20 07:51:33 +0800

    AC 1040-advanced

--- PAT (Advanced Level) Practise/1040. Longest Symmetric String (25).cpp
+++ PAT (Advanced Level) Practise/1040. Longest Symmetric String (25).cpp
@@ -1,22 +1,36 @@
+/**
+* 分析：经典DP模型之一 最长回文子串 dp[i][j] = dp[i+1][j-1]
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1000 + 5;
+const int maxn = 1010;
 
 int dp[maxn][maxn];
 
 int main() {
-    char s[maxn];
-    while(fgets(s, maxn, stdin)) {
-        memset(dp, 0, sizeof(dp));
-        int n = strlen(s);
-        int maxl = 0;
-        for(int i = n - 1; i >= 0; i--) //注意顺序
-            for(int j = i; j < n; j++)
-                if(s[i] == s[j] && (j - i + 1 < 3 || dp[i + 1][j - 1])) {
-                    dp[i][j] = 1;
-                    maxl = max(maxl, j - i + 1);
-                }
-        printf("%d\n", maxl);
+    int ans = 1;
+    string s;
+    getline(cin, s);
+    for(int i = 0; i < s.size(); i++) {
+        dp[i][i] = 1;
+        if(i != s.size() - 1) {
+            if(s[i] == s[i + 1]) {
+                dp[i][i + 1] = 1;
+                ans = 2;
+            }
+        }
     }
+    for(int L = 3; L <= s.size(); L++) { //枚举从3开始的每个长度，1和2已在设置边界时处理
+        for(int i = 0; i + L - 1 < s.size(); i++) {
+            int j = i + L - 1;
+            if(s[i] == s[j]) {
+                dp[i][j] = dp[i + 1][j - 1];
+                if(dp[i][j] == 1)
+                    ans = L;
+            }
+        }
+    }
+    printf("%d", ans);
     return 0;
 }
