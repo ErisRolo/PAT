e3cbde7b552e810272dc5e4e00b3cb747b00f36b
guohouxiao <122492508@qq.com>
2019-08-19 09:44:37 +0800

    new 1087-advanced

--- PAT (Advanced Level) Practise/1087. All Roads Lead to Rome (30).cpp
+++ PAT (Advanced Level) Practise/1087. All Roads Lead to Rome (30).cpp
@@ -1,40 +1,18 @@
 /**
-* 分析：Dijkstra+DFS，一小时内搞定，局部变量别忘了初始化
+* 分析：第一标尺cost仍为距离，第二标尺为点权的最短路径问题
+*       最短路径最后一题，35分钟AC，就是注意debug时候打的log记得删。。。
 **/
 
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn = 420;
+const int maxn = 210;
 const int inf = 1000000000;
 
-int n, k;
-int s;
-string st;
-int maxhappy = 0, numpath = 0;
-double avghappy = 0;
+int n, k, st, ed;
 int g[maxn][maxn];
-int d[maxn], h[maxn];
+int d[maxn];
 bool vis[maxn];
 vector<int> pre[maxn];
-vector<int> path, temppath;
-map<string, int> strtoid;
-map<int, string> idtostr;
-
-int num = 0;
-int getid(string str) {
-    if(strtoid.find(str) == strtoid.end()) {
-        strtoid[str] = num;
-        idtostr[num] = str;
-        return num++;
-    } else {
-        return strtoid[str];
-    }
-}
 
 void Dijkstra(int s) {
     fill(d, d + maxn, inf);
@@ -43,8 +21,8 @@ void Dijkstra(int s) {
         int u = -1, min = inf;
         for(int j = 0; j < n; j++) {
             if(vis[j] == false && d[j] < min) {
-                u = j;
                 min = d[j];
+                u = j;
             }
         }
         if(u == -1)
@@ -56,7 +34,7 @@ void Dijkstra(int s) {
                     d[v] = d[u] + g[u][v];
                     pre[v].clear();
                     pre[v].push_back(u);
-                } else if (d[u] + g[u][v] == d[v]) {
+                } else if(d[u] + g[u][v] == d[v]) {
                     pre[v].push_back(u);
                 }
             }
@@ -64,25 +42,32 @@ void Dijkstra(int s) {
     }
 }
 
+int num = 0, dist = 0;
+int maxhappy = 0, maxavghappy = 0;
+int h[maxn];
+vector<int> path, temppath;
 void DFS(int v) {
-    if(v == s) {
+    if(v == st) {
         temppath.push_back(v);
-        int hap = 0;
-        double avghap;
-        for(int i = temppath.size() - 1; i >= 0; i--) {
+        num++; //最短路径数+1
+        int happy = 0, avghappy = 0;
+        for(int i = temppath.size() - 1; i > 0; i--) { //计算最短路径
+            int id = temppath[i], nextid = temppath[i - 1];
+            dist += g[id][nextid];
+        }
+        for(int i = temppath.size() - 1; i >= 0; i--) { //计算点权
             int id = temppath[i];
-            hap += h[id];
+            happy += h[id];
         }
-        avghap  = hap * 1.0 / (temppath.size() - 1);
-        if(hap > maxhappy) {
-            maxhappy = hap;
+        avghappy = happy / (temppath.size() - 1);
+        if(happy > maxhappy) {
+            maxhappy = happy;
+            maxavghappy = avghappy;
             path = temppath;
-            avghappy = avghap;
-        } else if(hap == maxhappy && avghap > avghappy) {
+        } else if(happy == maxhappy && avghappy > maxavghappy) {
+            maxavghappy = avghappy;
             path = temppath;
-            avghappy = avghap;
         }
-        numpath++;
         temppath.pop_back();
         return;
     }
@@ -94,30 +79,43 @@ void DFS(int v) {
 
 int main() {
     fill(g[0], g[0] + maxn * maxn, inf);
-    scanf("%d%d", &n, &k);
-    cin >> st;
-    s = getid(st);
-    int c, happy;
-    string city;
-    for(int i = 1; i <= n - 1; i++) {
-        cin >> city >> happy;
-        c = getid(city);
-        h[c] = happy;
+    int cnt = 0, hp, cost;
+    char c[4], a[4], b[4];
+    string s, u, v;
+    map<string, int> id;
+    map<int, string> city;
+    scanf("%d %d %s", &n, &k, c);
+    s = c;
+    id[s] = cnt;
+    city[cnt] = s;
+    cnt++;
+    st = id[s]; //起点：0
+    for(int i = 0; i < n - 1; i++) {
+        scanf("%s %d", c, &hp);
+        s = c;
+        if(s != "ROM") {
+            id[s] = cnt;
+            city[cnt] = s;
+            cnt++;
+        } else {
+            id[s] = n - 1;
+            city[n - 1] = s;
+        }
+        h[id[s]] = hp;
     }
-    int a, b, cost;
-    string c1, c2;
+    ed = id["ROM"]; //终点：n-1
     for(int i = 0; i < k; i++) {
-        cin >> c1 >> c2 >> cost;
-        a = getid(c1);
-        b = getid(c2);
-        g[a][b] = g[b][a] = cost;
+        scanf("%s %s %d", a, b, &cost);
+        u = a, v = b;
+        g[id[a]][id[b]] = g[id[b]][id[a]] = cost;
     }
-    int e = getid("ROM");
-    Dijkstra(s);
-    DFS(e);
-    cout << numpath << " " << d[e] << " " << maxhappy << " " << (int)avghappy << endl;
-    for(int i = path.size() - 1; i > 0; i--)
-        cout << idtostr[path[i]] << "->";
-    cout << "ROM";
+    Dijkstra(st);
+    DFS(ed);
+    printf("%d %d %d %d\n", num, dist / num, maxhappy, maxavghappy);
+    for(int i = path.size() - 1; i >= 0; i--) {
+        printf("%s", city[path[i]].c_str());
+        if(i != 0)
+            printf("->");
+    }
     return 0;
 }
