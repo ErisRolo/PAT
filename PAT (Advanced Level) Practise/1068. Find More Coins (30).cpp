f654f71cd5fb9e9bc46cc9dfe741a034e5af86b5
guohouxiao <122492508@qq.com>
2019-08-20 16:43:17 +0800

    AC 1068-advanced

--- PAT (Advanced Level) Practise/1068. Find More Coins (30).cpp
+++ PAT (Advanced Level) Practise/1068. Find More Coins (30).cpp
@@ -1,49 +1,100 @@
+/**
+* 分析：归类为背包问题但我没看出来，用DFS只得了25分，有两个点TLE
+*       参照晴神标程，视为价值c[i]和质量w[i]等价的01背包问题
+*       靠没时间研究背包dp了，附一个吴佬提供的记忆化搜索的解法
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e4 + 10;
+const int maxn = 10010;
+const int maxv = 110;
 
-int n, m;
-int a[maxn], dp[110];
-bool choice[maxn][110], flag[maxn];
+int A[maxn];
+int dp[maxn][maxv];
+
+int dfs(int n, int m) {
+    if(n <= 0 || m <= 0)
+        return 0;
+    if(dp[n][m] != -1)
+        return dp[n][m];
+    else
+        dp[n][m] = max(dfs(n - 1, m), m >= A[n] ? dfs(n - 1, m - A[n]) + A[n] : 0);
+    return dp[n][m];
+}
 
 int main() {
+    fill(dp[0], dp[0] + maxn * maxv, -1);
+    int n, m;
     scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    sort(a + 1, a + n + 1, greater<int>());
-    for (int i = 1; i <= n; i++) {
-        for (int v = m; v >= a[i]; v--) {
-            if (dp[v - a[i]] + a[i] >= dp[v]) {
-                choice[i][v] = true;
-                dp[v] = dp[v - a[i]] + a[i];
-            } else {
-                choice[i][v] = false;
-                dp[v] = dp[v];
-            }
+    for(int i = 1; i <= n; i++)
+        scanf("%d", &A[i]);
+    sort(A + 1, A + n + 1, greater<int>());
+    dfs(n, m);
+    int res = m;
+    vector<int>ans;
+    for(int i = n; i > 0; i--) {
+        if(A[i] == res || dp[i - 1][res - A[i]] == res - A[i]) {
+            ans.push_back(A[i]);
+            res -= A[i];
         }
     }
-    if (dp[m] != m) {
+    if(res != 0)
         printf("No Solution");
-        return 0;
-    }
-    int k = n, cnt = 0;
-    while (k) {
-        if (choice[k][m]) {
-            flag[k] = true;
-            m -= a[k];
-            cnt++;
-        } else {
-            flag[k] = false;
-        }
-        k--;
-    }
-    for (int i = n; i >= 1; i--) {
-        if (flag[i]) {
-            printf("%d", a[i]);
-            cnt--;
-            if (cnt)
+    else {
+        for(int i = 0; i < ans.size(); i++) {
+            printf("%d", ans[i]);
+            if(i != ans.size() - 1)
                 printf(" ");
         }
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//int n,m;
+//int d[maxn];
+//vector<int> ans,temp;
+//
+//void DFS(int index,int sum) {
+//	if(sum==m) {
+//		if(ans.size()==0)
+//			ans=temp;
+//		else if(temp<ans)
+//			ans=temp;
+//		return;
+//	}
+//	if(sum>m||index>=n)
+//		return;
+//	temp.push_back(index);
+//	DFS(index+1,sum+d[index]);
+//	temp.pop_back();
+//	DFS(index+1,sum);
+//}
+//
+//int main() {
+//	scanf("%d%d",&n,&m);
+//	for(int i=0; i<n; i++)
+//		scanf("%d",&d[i]);
+//	sort(d,d+n);
+//	DFS(0,0);
+//	if(ans.size()!=0) {
+//		for(int i=0; i<ans.size(); i++) {
+//			printf("%d",d[ans[i]]);
+//			if(i!=ans.size()-1)
+//				printf(" ");
+//		}
+//	} else {
+//		printf("No Solution");
+//	}
+//	return 0;
+//}
