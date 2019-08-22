5650e6627b470171ab71fecc28efede150934967
guohouxiao <122492508@qq.com>
2019-08-22 08:53:26 +0800

    AC 1017-advanced

--- PAT (Advanced Level) Practise/1017. Queueing at Bank (25).cpp
+++ PAT (Advanced Level) Practise/1017. Queueing at Bank (25).cpp
@@ -1,59 +1,65 @@
-// 不需要用到队列
+/**
+* 分析：快乐模拟，给出到达时间和处理时间，计算平均等待时间，其实这道题过程还是比较清晰的
+*       题目里说超过17:00未被服务的客户不被计算在客户中，然而没有处理这一逻辑
+*       如minEndTime算出17:01，仍会计算其等待时间
+*       估计题目想说的意思仅是17:00未到达的客户不被计算
+**/
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+const int maxn = 10010;
+const int maxk = 110;
+const int inf = 1000000000;
 
-struct cust {
-    int arrive;    //到达时间
-    int process;   //处理时间
-    int start;     //开始处理时间
-    int leave;     //结束处理时
-    bool operator < (const cust &a)const {//重载<操作符，用于排序
-        return arrive < a.arrive;
-    }
+struct customer {
+    int t, p; //到达时间 处理时间
 };
-int main() {
-    int N, K;
-    scanf("%d%d", &N, &K);
-    vector<cust> CUST(N);
-    int h, m, s, pro;
-    // 把时间都化为秒哦, 这样能够方便计算呢!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    for (int i = 0; i < N; i++) { //化为秒
-        scanf("%d:%d:%d %d", &h, &m, &s, &pro);
-        CUST[i].arrive = (h * 60 + m) * 60 + s;
-        CUST[i].process = pro * 60;
-    }
-    sort(CUST.begin(), CUST.end());//按照重载后的<操作符排序
 
-    vector<int> window(K, 8 * 60 * 60); //窗口初始时间
-    int quickTime;
-    int quickWindow;
-    for (int i = 0; i < N; i++) { //选择窗口的条件: 每次选择等待时间最短的窗口!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        quickTime = 2147483647;
-        for (int j = 0; j < K; j++) {
-            if (window[j] < quickTime) {
-                quickTime = window[j];
-                quickWindow = j;
+bool cmp(customer a, customer b) {
+    return a.t < b.t;
+}
+
+vector<customer> c; //有效客户队列
+int endTime[maxk]; //endTime[i]：i号窗口当前客户的服务结束时间
+
+int main() {
+    int n, k, ans = 0;
+    int h, m, s, p;
+    int early = 8 * 3600, late = 17 * 3600;
+    scanf("%d%d", &n, &k);
+    fill(endTime, endTime + k, early); //每个窗口的服务结束时间初始化为营业时间
+    for(int i = 0; i < n; i++) {
+        scanf("%d:%d:%d %d", &h, &m, &s, &p);
+        int t = h * 3600 + m * 60 + s;
+        if(p > 60)
+            p = 60;
+        p *= 60;
+        if(t <= late) {
+            customer temp;
+            temp.t = t, temp.p = p;
+            c.push_back(temp);
+        }
+    }
+    sort(c.begin(), c.end(), cmp);
+    for(int i = 0; i < c.size(); i++) { //计算每个客户的等待时间
+        int idx = -1, minEndTime = inf; //查找当前最早服务结束的窗口 并记录时间
+        for(int j = 0; j < k; j++) {
+            if(endTime[j] < minEndTime) {
+                minEndTime = endTime[j];
+                idx = j;
             }
         }
-        CUST[i].start = max(window[quickWindow], CUST[i].arrive);//需要等待和无需等待两种情况
-        CUST[i].leave = CUST[i].start + CUST[i].process;
-        window[quickWindow] = CUST[i].leave;
-    }
-    double sum = 0;
-    int count = 0;
-    for (int i = 0; i < N; i++) {
-        if (CUST[i].arrive <= 17 * 60 * 60) {//17点前来的
-            sum += CUST[i].start - CUST[i].arrive;
-            count++;
+        if(c[i].t >= minEndTime) { //如果到达时间晚于最早服务结束时间，直接分配窗口，更新服务结束时间
+            endTime[idx] = c[i].t + c[i].p;
+        } else {
+            ans += minEndTime - c[i].t;
+            //endTime[idx]=c[i].t+(minEndTime-c[i].t)+c[i].p;
+            endTime[idx] += c[i].p;
         }
     }
-    if (sum > 0)
-        printf("%.1f", sum / 60 / count);
-    else
+    if(c.size() == 0)
         printf("0.0");
+    else
+        printf("%.1f", ans / 60.0 / c.size());
     return 0;
 }
