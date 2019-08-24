8d0762eeceedce834f4de4512bf5e66970f3a1f1
guohouxiao <122492508@qq.com>
2019-08-24 18:18:23 +0800

    AC 1113-advanced

--- PAT (Advanced Level) Practise/1113. Integer Set Partition (25).cpp
+++ PAT (Advanced Level) Practise/1113. Integer Set Partition (25).cpp
@@ -1,23 +1,29 @@
+/**
+* ·ÖÎö£º¼òµ¥Ìâ
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 100001;
+const int maxn = 100010;
 
-int a[maxn];
+int d[maxn];
 
 int main() {
-    int n, s1 = 0, s2 = 0;
-    cin >> n;
+    int n, n1, n2;
+    int t1 = 0, t2 = 0, ans;
+    scanf("%d", &n);
     for(int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &d[i]);
     }
-    sort(a, a + n);
-    // nth_element(a,a+n/2,a+n);
-    for(int i = 0; i < n; i++) {
-        if(i < n / 2)
-            s1 += a[i];
-        else
-            s2 += a[i];
-    }
-    cout << n % 2 << ' ' << s2 - s1 << endl;
+    sort(d, d + n);
+    if(n % 2 == 0)
+        n1 = n2 = n / 2;
+    else
+        n1 = n / 2, n2 = n / 2 + 1;
+    for(int i = 0; i < n1; i++)
+        t1 += d[i];
+    for(int i = n1; i < n; i++)
+        t2 += d[i];
+    printf("%d %d", abs(n1 - n2), abs(t1 - t2));
     return 0;
 }
