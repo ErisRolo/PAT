8f9906ab5528dd5cc8014bb08eed9590e454b351
guohouxiao <122492508@qq.com>
2019-08-26 16:25:09 +0800

    AC 1125-advanced

--- PAT (Advanced Level) Practise/1125. Chain the Ropes (25).cpp
+++ PAT (Advanced Level) Practise/1125. Chain the Ropes (25).cpp
@@ -1,18 +1,22 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int maxn = 1e4 + 10;
+/**
+* 分析：chain的操作实际就是两个数相加除以2
+**/
 
-int a[maxn], n;
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 10010;
+
+double d[maxn];
 
 int main() {
+    int n;
     scanf("%d", &n);
     for(int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-    sort(a, a + n);
-    int ans = a[0];
-    for(int i = 1; i < n; i++) {
-        ans = (ans + a[i]) / 2;
-    }
+        scanf("%lf", &d[i]);
+    sort(d, d + n);
+    int ans = (int)((d[0] + d[1]) / 2);
+    for(int i = 2; i < n; i++)
+        ans = (int)((ans + d[i]) / 2);
     printf("%d", ans);
     return 0;
 }
