00edb97369d17e952f54205fb2011dc035a22623
guohouxiao <122492508@qq.com>
2019-08-27 20:17:02 +0800

    AC 1130-advanced

--- PAT (Advanced Level) Practise/1130. Infix Expression (25).cpp
+++ PAT (Advanced Level) Practise/1130. Infix Expression (25).cpp
@@ -1,35 +1,60 @@
-#include<bits/stdc++.h>
+/**
+* 分析：中缀表达式，中序遍历解决，可以明确不会出现只有左孩子的结点
+*       规律是有孩子结点的非根结点（无论是两个孩子都有还是只有右孩子），遍历完成后在左右加上括号
+**/
+
+#include <bits/stdc++.h>
 using namespace std;
+const int maxn = 21;
 
-struct node {
+int n, root;
+bool isRoot[maxn];
+vector<string> ans;
+
+struct Node {
     string data;
-    int l, r;
-} a[100];
+    int lchild, rchild;
+} node[maxn];
 
-string dfs(int root) {
-    if(a[root].l == -1 && a[root].r == -1)
-        return a[root].data;
-    if(a[root].l == -1 && a[root].r != -1)
-        return "(" +  a[root].data + dfs(a[root].r) + ")";
-    if(a[root].l != -1 && a[root].r != -1)
-        return "(" +  dfs(a[root].l) + a[root].data + dfs(a[root].r) + ")";
+void inOrder(int root) {
+    if(root == -1)
+        return;
+    if(isRoot[root] == false && node[root].rchild != -1) { //如果不是根结点且有孩子（右孩子）
+        ans.push_back("(");
+        inOrder(node[root].lchild);
+        ans.push_back(node[root].data);
+        inOrder(node[root].rchild);
+        ans.push_back(")");
+    } else {
+        inOrder(node[root].lchild);
+        ans.push_back(node[root].data);
+        inOrder(node[root].rchild);
+    }
 }
 
 int main() {
-    int have[100] = {0}, n, root = 1;
+    fill(isRoot, isRoot + maxn, true);
+    string s;
+    int l, r;
     cin >> n;
-    for(int i  = 1; i <= n; i++) {
-        cin >> a[i].data >> a[i].l >> a[i].r;
-        if(a[i].l != -1)
-            have[a[i].l] = 1;
-        if(a[i].r != -1)
-            have[a[i].r] = 1;
+    for(int i = 1; i <= n; i++) {
+        cin >> s >> l >> r;
+        node[i].data = s;
+        node[i].lchild = l;
+        node[i].rchild = r;
+        if(l != -1)
+            isRoot[l] = false;
+        if(r != -1)
+            isRoot[r] = false;
     }
-    while(have[root] == 1)
-        root++;
-    string ans = dfs(root);
-    if(ans[0] == '(')
-        ans = ans.substr(1, ans.size() - 2);
-    cout << ans;
+    for(int i = 1; i <= n; i++) {
+        if(isRoot[i] == true) {
+            root = i;
+            break;
+        }
+    }
+    inOrder(root);
+    for(auto s : ans)
+        cout << s;
     return 0;
 }
