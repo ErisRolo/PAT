8307ee9202115a2943ccf8b2a383bff72be4af3b
guohouxiao <122492508@qq.com>
2019-08-27 17:47:37 +0800

    AC 1128-advanced

--- PAT (Advanced Level) Practise/1128. N Queens Puzzle (20).cpp
+++ PAT (Advanced Level) Practise/1128. N Queens Puzzle (20).cpp
@@ -1,23 +1,33 @@
+/**
+* 分析：N皇后问题，并没有求解，只是判断，easy
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 1010;
+
+int col[maxn]; //第i列的棋子所在的行数
 
 int main() {
     int k, n;
-    cin >> k;
-    for (int i = 0; i < k; i++) {
-        cin >> n;
-        vector<int> v(n);
-        bool result = true;
-        for (int j = 0; j < n; j++) {
-            cin >> v[j];
-            for (int t = 0; t < j; t++) {
-                if (v[j] == v[t] || abs(v[j] - v[t]) == abs(j - t)) {
-                    result = false;
+    scanf("%d", &k);
+    while(k--) { //依次输入每列棋子所在的行数（从下往上数）
+        scanf("%d", &n);
+        for(int i = 1; i <= n; i++)
+            scanf("%d", &col[i]);
+        bool flag = true;
+        for(int i = 1; i <= n; i++) {
+            for(int j = i + 1; j <= n; j++) {
+                if(abs(j - i) == abs(col[j] - col[i]) || col[j] == col[i]) {
+                    flag = false;
                     break;
                 }
             }
         }
-        cout << (result == true ? "YES\n" : "NO\n");
+        if(flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
