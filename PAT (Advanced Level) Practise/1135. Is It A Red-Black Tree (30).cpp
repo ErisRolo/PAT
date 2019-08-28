839eff3964ae743f52a16090c1e99d6ebe53ad60
guohouxiao <122492508@qq.com>
2019-08-28 20:25:28 +0800

    AC 1135-advanced

--- PAT (Advanced Level) Practise/1135. Is It A Red-Black Tree (30).cpp
+++ PAT (Advanced Level) Practise/1135. Is It A Red-Black Tree (30).cpp
@@ -1,81 +1,81 @@
+/**
+* 分析：红黑树，给出的是先序遍历，可以直接插入建树
+*       然后判断根结点是否为黑色
+*       接着判断每个红色结点是否有孩子结点，有的话是否为黑色
+*       最后一个条件是指判断每个结点，从其出发到达的所有叶子结点的路径上经过的黑色结点数目是否相等
+*       前两个很容易，最后一个，先写一个递归函数getnum用于获取黑色结点数目，然后用DFS或BFS遍历判断每个结点即可
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 31;
 
-struct node {
-    int val;
-    struct node *lchild, *rchild;
-};
+int k, n, d, root = -1;
 
-//建树
-void build(node* &root, int v) {
-    if (root == NULL) {
-        root = new node();
-        root->lchild = root->rchild = NULL;
-        root->val = v;
+struct Node {
+    int data, lchild, rchild;
+} node[maxn];
+
+int idx = 0;
+void insert(int &root, int x) {
+    if(root == -1) {
+        node[idx].data = x;
+        node[idx].lchild = node[idx].rchild = -1;
+        root = idx++;
         return;
-    } else if(abs(v) <= abs(root->val))
-        build(root->lchild, v);
+    }
+    if(abs(x) < abs(node[root].data)) {
+        insert(node[root].lchild, x);
+    } else {
+        insert(node[root].rchild, x);
+    }
+}
+
+int getnum(int root) {
+    if(root == -1)
+        return 0;
+    int lnum = getnum(node[root].lchild);
+    int rnum = getnum(node[root].rchild);
+    if(node[root].data > 0)
+        return max(lnum, rnum) + 1;
     else
-        build(root->rchild, v);
+        return max(lnum, rnum);
 }
 
-//temp记录黑的个数
-void DFS(node* root, int temp, int &black) {
-    //叶节点
-    if(root == NULL) {
-        if(black == -1)
-            black = temp;
-        else if(black != temp)
-            black = -2;
+bool flag;
+void DFS(int root) {
+    if(root == -1)
         return;
+    int l = node[root].lchild, r = node[root].rchild;
+    if(node[root].data < 0) { //判断2
+        if(l != -1 && node[l].data < 0)
+            flag = false;
+        if(r != -1 && node[r].data < 0)
+            flag = false;
     }
-    if(black == -2)
-        return;
-    if(root->val > 0)
-        temp++;
-    DFS(root->lchild, temp, black);
-    DFS(root->rchild, temp, black);
-}
-
-bool BFS(node* root) {
-    queue<node*> q;
-    q.push(root);
-    if(root->val < 0)
-        return false;//根结点是红
-    while(!q.empty()) {
-        node* top = q.front();
-        q.pop();
-        int black = -1;
-        DFS(top, 0, black);
-        if(black == -2)
-            return false;
-        if(top->lchild != NULL) {
-            if(top->val < 0 && top->lchild->val < 0)
-                return false;
-            else
-                q.push(top->lchild);
-        }
-        if(top->rchild != NULL) {
-            if(top->val < 0 && top->rchild->val < 0)
-                return false;
-            else
-                q.push(top->rchild);
-        }
-    }
-    return true;
+    //判断3
+    int lnum = getnum(l), rnum = getnum(r);
+    if(lnum != rnum)
+        flag = false;
+    DFS(l);
+    DFS(r);
 }
 
 int main() {
-    int k, n, v;
     scanf("%d", &k);
-    for(int i = 0; i < k; i++) {
+    while(k--) {
+        root = -1, idx = 0;
+        flag = true;
         scanf("%d", &n);
-        node *root = NULL;
-        for(int j = 0; j < n; j++) {
-            scanf("%d", &v);
-            build(root, v);
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &d);
+            insert(root, d);
         }
-        if(BFS(root) == true)
+        //判断1
+        if(node[root].data < 0)
+            flag = false;
+        DFS(root); //DFS过程中进行判断2和判断3
+        if(flag)
             printf("Yes\n");
         else
             printf("No\n");
