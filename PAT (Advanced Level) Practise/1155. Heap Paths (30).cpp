98d8aec4d68c4dbca96b5d1def8a9b737ce28f07
guohouxiao <122492508@qq.com>
2019-08-31 19:39:26 +0800

    new 1155-advanced

--- PAT (Advanced Level) Practise/1155. Heap Paths (30).cpp
+++ PAT (Advanced Level) Practise/1155. Heap Paths (30).cpp
@@ -1,62 +1,150 @@
 /**
-* 分析：不是堆排序，堆的概念题
-*       从根结点进行DFS，遇到叶子结点就打印路径
-*       根据左右子树分两种情况进行递归即可
+* 分析：仿照图的路径写了新的DFS
+*       对于堆的判断目前一共出现3种写法了，1147一种，这道题下面的两种，随便掌握一种即可
 **/
 
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1010;
 
-int n, heap[maxn];
-bool isMax = true, isMin = true;
-vector<int> path;
+int n;
+int heap[maxn];
 
-void DFS(int v) {
-    //递归边界：到达叶子结点
-    if(v * 2 > n && v * 2 + 1 > n) {
+vector<int> path, temppath;
+void DFS(int index) {
+    if(index * 2 > n) {
+        temppath.push_back(index);
+        path = temppath;
         for(int i = 0; i < path.size(); i++) {
-            printf("%d", path[i]);
+            printf("%d", heap[path[i]]);
             if(i != path.size() - 1)
                 printf(" ");
             else
                 printf("\n");
         }
+        temppath.pop_back();
+        return;
     }
-    //既有左子树也有右子树
-    if(v * 2 <= n && v * 2 + 1 <= n) {
-        path.push_back(heap[v * 2 + 1]);
-        DFS(v * 2 + 1);
-        path.pop_back();
-        path.push_back(heap[v * 2]);
-        DFS(v * 2);
-        path.pop_back();
-    }
-    //只有左子树没有右子树
-    if(v * 2 <= n && v * 2 + 1 > n) {
-        path.push_back(heap[v * 2]);
-        DFS(v * 2);
-        path.pop_back();
-    }
+    temppath.push_back(index);
+    if(index * 2 + 1 <= n)
+        DFS(index * 2 + 1);
+    if(index * 2 <= n)
+        DFS(index * 2);
+    temppath.pop_back();
 }
 
 int main() {
     scanf("%d", &n);
     for(int i = 1; i <= n; i++)
         scanf("%d", &heap[i]);
-    path.push_back(heap[1]);
     DFS(1);
-    for(int i = 2; i <= n; i++) {
-        if(heap[i / 2] < heap[i])
-            isMax = false;
-        if(heap[i / 2] > heap[i])
-            isMin = false;
+    bool isheap = true, ismax = false, ismin = false;
+    if(isheap && !ismax && !ismin) {
+        if(heap[1] > heap[2])
+            ismax = true;
+        else if(heap[1] < heap[2])
+            ismin = true;
     }
-    if(isMax)
-        printf("Max Heap");
-    if(isMin)
-        printf("Min Heap");
-    if(!isMax && !isMin)
-        printf("Not Heap");
+    if(ismax) {
+        for(int i = 1; i <= n / 2; i++) {
+            if(heap[i] < heap[i * 2]) {
+                isheap = false;
+                break;
+            }
+        }
+    } else if(ismin) {
+        for(int i = 1; i <= n / 2; i++) {
+            if(heap[i] > heap[i * 2]) {
+                isheap = false;
+                break;
+            }
+        }
+    }
+    if(isheap) {
+        if(ismax)
+            printf("Max Heap\n");
+        else if(ismin)
+            printf("Min Heap\n");
+    } else
+        printf("Not Heap\n");
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**
+//* 分析：不是堆排序，堆的概念题
+//*       从根结点进行DFS，遇到叶子结点就打印路径
+//*       根据左右子树分两种情况进行递归即可
+//**/
+//
+//#include <bits/stdc++.h>
+//using namespace std;
+//const int maxn = 1010;
+//
+//int n, heap[maxn];
+//bool isMax = true, isMin = true;
+//vector<int> path;
+//
+//void DFS(int v) {
+//    //递归边界：到达叶子结点
+//    if(v * 2 > n && v * 2 + 1 > n) {
+//        for(int i = 0; i < path.size(); i++) {
+//            printf("%d", path[i]);
+//            if(i != path.size() - 1)
+//                printf(" ");
+//            else
+//                printf("\n");
+//        }
+//    }
+//    //既有左子树也有右子树
+//    if(v * 2 <= n && v * 2 + 1 <= n) {
+//        path.push_back(heap[v * 2 + 1]);
+//        DFS(v * 2 + 1);
+//        path.pop_back();
+//        path.push_back(heap[v * 2]);
+//        DFS(v * 2);
+//        path.pop_back();
+//    }
+//    //只有左子树没有右子树
+//    if(v * 2 <= n && v * 2 + 1 > n) {
+//        path.push_back(heap[v * 2]);
+//        DFS(v * 2);
+//        path.pop_back();
+//    }
+//}
+//
+//int main() {
+//    scanf("%d", &n);
+//    for(int i = 1; i <= n; i++)
+//        scanf("%d", &heap[i]);
+//    path.push_back(heap[1]);
+//    DFS(1);
+//    for(int i = 2; i <= n; i++) {
+//        if(heap[i / 2] < heap[i])
+//            isMax = false;
+//        if(heap[i / 2] > heap[i])
+//            isMin = false;
+//    }
+//    if(isMax)
+//        printf("Max Heap");
+//    if(isMin)
+//        printf("Min Heap");
+//    if(!isMax && !isMin)
+//        printf("Not Heap");
+//    return 0;
+//}
